67779433365c8388b7b49927e7070a61670064d5
raghawardhan585 <raghawardhan585@gmail.com>
2020-11-03 09:58:50 -0800

    Optic Tensor Run

--- optic_tensor_runs.sh
+++ optic_tensor_runs.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' True > RunGpu_0.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' False > RunGpu_1.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:2' False > RunGpu_2.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:3' False > RunGpu_3.txt &
-
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' True > Run_info/Run_0.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' False > Run_info/Run_1.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:2' False > Run_info/Run_2.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:3' False > Run_info/Run_3.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' False > Run_info/Run_4.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' False > Run_info/Run_5.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:2' False > Run_info/Run_6.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:3' False > Run_info/Run_7.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' False > Run_info/Run_8.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' False > Run_info/Run_9.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:2' False > Run_info/Run_10.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:3' False > Run_info/Run_11.txt &
+Echo "Running all sessions"
+wait
 echo "All sessions are complete"
