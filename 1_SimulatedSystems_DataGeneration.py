a8863ecbbc3a0ed25ffef9d7b64b6410cdd054cf
raghawardhan585 <raghawardhan585@gmail.com>
2020-11-08 09:28:48 -0800

    MT runs

--- 1_SimulatedSystems_DataGeneration.py
+++ 1_SimulatedSystems_DataGeneration.py
@@ -60,16 +60,16 @@ oc.data_gen_sys_1_2(sys_params, N_CURVES, SYSTEM_NO)
 
 ## Bash Script Generator
 
-# DEVICE_TO_RUN_ON = 'microtensor'
+DEVICE_TO_RUN_ON = 'microtensor'
 # DEVICE_TO_RUN_ON = 'optictensor'
-DEVICE_TO_RUN_ON = 'goldentensor'
+# DEVICE_TO_RUN_ON = 'goldentensor'
 DATA_SYSTEM_TO_WRITE_BASH_SCRIPT_FOR = 2
 NO_OF_ITERATIONS_PER_GPU = 5
 NO_OF_ITERATIONS_IN_CPU = 5
 
 dict_run_conditions = {}
 # MICROTENSOR CPU RUN
-# dict_run_conditions[0] = {'x_dict_size':6,'x_nn_layers':4,'x_nn_nodes':18}
+dict_run_conditions[0] = {'x_dict_size':1,'x_nn_layers':5,'x_nn_nodes':18}
 # dict_run_conditions[1] = {'x_dict_size':2,'x_nn_layers':5,'x_nn_nodes':9}
 # dict_run_conditions[2] = {'x_dict_size':2,'x_nn_layers':5,'x_nn_nodes':12}
 # dict_run_conditions[3] = {'x_dict_size':2,'x_nn_layers':5,'x_nn_nodes':15}
@@ -79,8 +79,8 @@ dict_run_conditions = {}
 
 # dict_run_conditions[6] = {'x_dict_size':4,'x_nn_layers':5,'x_nn_nodes':9}
 # Runs
-dict_run_conditions[0] = {'x_dict_size':3,'x_nn_layers':5,'x_nn_nodes':18}
-dict_run_conditions[1] = {'x_dict_size':4,'x_nn_layers':5,'x_nn_nodes':6}
+# dict_run_conditions[0] = {'x_dict_size':3,'x_nn_layers':5,'x_nn_nodes':18}
+# dict_run_conditions[1] = {'x_dict_size':4,'x_nn_layers':5,'x_nn_nodes':6}
 # dict_run_conditions[2] = {'x_dict_size':3,'x_nn_layers':5,'x_nn_nodes':12}
 # dict_run_conditions[3] = {'x_dict_size':3,'x_nn_layers':5,'x_nn_nodes':15}
 
