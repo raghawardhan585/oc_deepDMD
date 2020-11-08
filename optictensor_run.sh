7efc4be7f9c38aabb9747e1f2179a5eb725af5d1
raghawardhan585 <raghawardhan585@gmail.com>
2020-11-08 09:48:01 -0800

    OT and FT runs

--- optictensor_run.sh
+++ optictensor_run.sh
@@ -4,26 +4,16 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [with_u] [with_y] [mix_xu] [run_no] [dict_size] [nn_layers] [nn_nodes] [write_to_file] 
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' 2 0 1 0 0 3 5 6 > Run_info/SYS_2_RUN_0.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' 2 0 1 0 1 3 5 6 > Run_info/SYS_2_RUN_1.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' 2 0 1 0 2 3 5 6 > Run_info/SYS_2_RUN_2.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' 2 0 1 0 3 3 5 6 > Run_info/SYS_2_RUN_3.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' 2 0 1 0 4 3 5 6 > Run_info/SYS_2_RUN_4.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' 2 0 1 0 5 3 5 9 > Run_info/SYS_2_RUN_5.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' 2 0 1 0 6 3 5 9 > Run_info/SYS_2_RUN_6.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' 2 0 1 0 7 3 5 9 > Run_info/SYS_2_RUN_7.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' 2 0 1 0 8 3 5 9 > Run_info/SYS_2_RUN_8.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' 2 0 1 0 9 3 5 9 > Run_info/SYS_2_RUN_9.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:2' 2 0 1 0 10 3 5 12 > Run_info/SYS_2_RUN_10.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:2' 2 0 1 0 11 3 5 12 > Run_info/SYS_2_RUN_11.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:2' 2 0 1 0 12 3 5 12 > Run_info/SYS_2_RUN_12.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:2' 2 0 1 0 13 3 5 12 > Run_info/SYS_2_RUN_13.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:2' 2 0 1 0 14 3 5 12 > Run_info/SYS_2_RUN_14.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:3' 2 0 1 0 15 3 5 15 > Run_info/SYS_2_RUN_15.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:3' 2 0 1 0 16 3 5 15 > Run_info/SYS_2_RUN_16.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:3' 2 0 1 0 17 3 5 15 > Run_info/SYS_2_RUN_17.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:3' 2 0 1 0 18 3 5 15 > Run_info/SYS_2_RUN_18.txt &
-python3 gen_control_Koopman_SHARA_addition.py '/gpu:3' 2 0 1 0 19 3 5 15 > Run_info/SYS_2_RUN_19.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' 2 0 1 0 0 2 5 18 > Run_info/SYS_2_RUN_0.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' 2 0 1 0 1 2 5 18 > Run_info/SYS_2_RUN_1.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' 2 0 1 0 2 2 5 18 > Run_info/SYS_2_RUN_2.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' 2 0 1 0 3 2 5 18 > Run_info/SYS_2_RUN_3.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:0' 2 0 1 0 4 2 5 18 > Run_info/SYS_2_RUN_4.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' 2 0 1 0 5 4 5 9 > Run_info/SYS_2_RUN_5.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' 2 0 1 0 6 4 5 9 > Run_info/SYS_2_RUN_6.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' 2 0 1 0 7 4 5 9 > Run_info/SYS_2_RUN_7.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' 2 0 1 0 8 4 5 9 > Run_info/SYS_2_RUN_8.txt &
+python3 gen_control_Koopman_SHARA_addition.py '/gpu:1' 2 0 1 0 9 4 5 9 > Run_info/SYS_2_RUN_9.txt &
 echo "Running all sessions" 
 wait 
 echo "All sessions are complete" 
