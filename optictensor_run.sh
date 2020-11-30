adb64816a4aa5f4ca09e968d639ba0e1d27a7c56
raghawardhan585 <raghawardhan585@gmail.com>
2020-11-30 02:45:40 -0800

    Next runs

--- optictensor_run.sh
+++ optictensor_run.sh
@@ -4,14 +4,14 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [x_dict] [x_layers] [x_nodes] [y_dict] [y_layers] [y_nodes] [xy_dict] [xy_layers] [xy_nodes] [write_to_file] 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 21 0 5 3 9 1 3 9 1 3 9 > Run_info/SYS_21_RUN_0.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:0' 21 1 5 3 9 1 3 9 1 3 9 > Run_info/SYS_21_RUN_1.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 21 2 5 4 9 1 4 9 1 4 9 > Run_info/SYS_21_RUN_2.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 21 3 5 4 9 1 4 9 1 4 9 > Run_info/SYS_21_RUN_3.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 21 4 5 5 9 1 5 9 1 5 9 > Run_info/SYS_21_RUN_4.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 21 5 5 5 9 1 5 9 1 5 9 > Run_info/SYS_21_RUN_5.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 21 6 5 6 9 1 6 9 1 6 9 > Run_info/SYS_21_RUN_6.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 21 7 5 6 9 1 6 9 1 6 9 > Run_info/SYS_21_RUN_7.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:0' 21 0 6 3 9 1 3 9 1 3 9 > Run_info/SYS_21_RUN_0.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:0' 21 1 6 3 9 1 3 9 1 3 9 > Run_info/SYS_21_RUN_1.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:1' 21 2 6 4 9 1 4 9 1 4 9 > Run_info/SYS_21_RUN_2.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:1' 21 3 6 4 9 1 4 9 1 4 9 > Run_info/SYS_21_RUN_3.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:2' 21 4 6 5 9 1 5 9 1 5 9 > Run_info/SYS_21_RUN_4.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:2' 21 5 6 5 9 1 5 9 1 5 9 > Run_info/SYS_21_RUN_5.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:3' 21 6 6 6 9 1 6 9 1 6 9 > Run_info/SYS_21_RUN_6.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:3' 21 7 6 6 9 1 6 9 1 6 9 > Run_info/SYS_21_RUN_7.txt &
 echo "Running all sessions" 
 wait 
 echo "All sessions are complete" 
