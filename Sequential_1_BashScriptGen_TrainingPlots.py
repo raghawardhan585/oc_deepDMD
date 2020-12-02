b24350d340a5682b830583424c1a765199f3da29
raghawardhan585 <raghawardhan585@gmail.com>
2020-12-02 09:25:41 -0800

    Next runs

--- Sequential_1_BashScriptGen_TrainingPlots.py
+++ Sequential_1_BashScriptGen_TrainingPlots.py
@@ -24,23 +24,23 @@ colors = np.asarray(colors);  # defines a color palette
 
 
 ## Bash Script Generation
-DEVICE_TO_RUN_ON = 'microtensor'
+# DEVICE_TO_RUN_ON = 'microtensor'
 # DEVICE_TO_RUN_ON = 'optictensor'
-# DEVICE_TO_RUN_ON = 'goldentensor'
+DEVICE_TO_RUN_ON = 'goldentensor'
 DATA_SYSTEM_TO_WRITE_BASH_SCRIPT_FOR = 23
-NO_OF_ITERATIONS_PER_GPU = 2
-NO_OF_ITERATIONS_IN_CPU = 2
+NO_OF_ITERATIONS_PER_GPU = 1
+NO_OF_ITERATIONS_IN_CPU = 1
 dict_run_conditions = {}
 
 # MICROTENSOR CPU RUN
-dict_run_conditions[0] = {}
-dict_run_conditions[0]['x']  = {'dict_size':9,'nn_layers':6,'nn_nodes':9}
-dict_run_conditions[0]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':3}
-dict_run_conditions[0]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':3}
-dict_run_conditions[1] = {}
-dict_run_conditions[1]['x']  = {'dict_size':9,'nn_layers':7,'nn_nodes':9}
-dict_run_conditions[1]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
-dict_run_conditions[1]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
+# dict_run_conditions[0] = {}
+# dict_run_conditions[0]['x']  = {'dict_size':9,'nn_layers':6,'nn_nodes':9}
+# dict_run_conditions[0]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':3}
+# dict_run_conditions[0]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':3}
+# dict_run_conditions[1] = {}
+# dict_run_conditions[1]['x']  = {'dict_size':9,'nn_layers':7,'nn_nodes':9}
+# dict_run_conditions[1]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
+# dict_run_conditions[1]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
 
 # dict_run_conditions[2] = {}
 # dict_run_conditions[2]['x']  = {'dict_size':9,'nn_layers':9,'nn_nodes':15}
@@ -52,38 +52,38 @@ dict_run_conditions[1]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
 # dict_run_conditions[3]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':12}
 
 # Golden tensor
-# dict_run_conditions[0] = {}
-# dict_run_conditions[0]['x']  = {'dict_size':9,'nn_layers':8,'nn_nodes':9}
-# dict_run_conditions[0]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':3}
-# dict_run_conditions[0]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':3}
-# dict_run_conditions[1] = {}
-# dict_run_conditions[1]['x']  = {'dict_size':9,'nn_layers':9,'nn_nodes':9}
-# dict_run_conditions[1]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
-# dict_run_conditions[1]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
-# dict_run_conditions[2] = {}
-# dict_run_conditions[2]['x']  = {'dict_size':9,'nn_layers':10,'nn_nodes':9}
-# dict_run_conditions[2]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':9}
-# dict_run_conditions[2]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':9}
-# dict_run_conditions[3] = {}
-# dict_run_conditions[3]['x']  = {'dict_size':9,'nn_layers':6,'nn_nodes':10}
-# dict_run_conditions[3]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':12}
-# dict_run_conditions[3]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':12}
+dict_run_conditions[0] = {}
+dict_run_conditions[0]['x']  = {'dict_size':9,'nn_layers':6,'nn_nodes':9}
+dict_run_conditions[0]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':3}
+dict_run_conditions[0]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':3}
+dict_run_conditions[1] = {}
+dict_run_conditions[1]['x']  = {'dict_size':9,'nn_layers':6,'nn_nodes':9}
+dict_run_conditions[1]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
+dict_run_conditions[1]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
+dict_run_conditions[2] = {}
+dict_run_conditions[2]['x']  = {'dict_size':9,'nn_layers':6,'nn_nodes':9}
+dict_run_conditions[2]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':9}
+dict_run_conditions[2]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':9}
+dict_run_conditions[3] = {}
+dict_run_conditions[3]['x']  = {'dict_size':9,'nn_layers':6,'nn_nodes':9}
+dict_run_conditions[3]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':12}
+dict_run_conditions[3]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':12}
 
 # Optic tensor
 # dict_run_conditions[0] = {}
-# dict_run_conditions[0]['x']  = {'dict_size':9,'nn_layers':7,'nn_nodes':10}
+# dict_run_conditions[0]['x']  = {'dict_size':9,'nn_layers':6,'nn_nodes':9}
 # dict_run_conditions[0]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':4}
 # dict_run_conditions[0]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':4}
 # dict_run_conditions[1] = {}
-# dict_run_conditions[1]['x']  = {'dict_size':9,'nn_layers':8,'nn_nodes':10}
+# dict_run_conditions[1]['x']  = {'dict_size':9,'nn_layers':6,'nn_nodes':9}
 # dict_run_conditions[1]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
 # dict_run_conditions[1]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
 # dict_run_conditions[2] = {}
-# dict_run_conditions[2]['x']  = {'dict_size':9,'nn_layers':9,'nn_nodes':10}
+# dict_run_conditions[2]['x']  = {'dict_size':9,'nn_layers':6,'nn_nodes':9}
 # dict_run_conditions[2]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':9}
 # dict_run_conditions[2]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':9}
 # dict_run_conditions[3] = {}
-# dict_run_conditions[3]['x']  = {'dict_size':9,'nn_layers':10,'nn_nodes':10}
+# dict_run_conditions[3]['x']  = {'dict_size':9,'nn_layers':6,'nn_nodes':9}
 # dict_run_conditions[3]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':12}
 # dict_run_conditions[3]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':12}
 
@@ -95,7 +95,7 @@ seq.transfer_current_ocDeepDMD_run_files()
 
 ##
 SYSTEM_NO = 23
-ls_process_runs = list(range(0,92))
+ls_process_runs = list(range(92,112))
 seq.generate_predictions_pickle_file(SYSTEM_NO,state_only =True,ls_process_runs=ls_process_runs)
 seq.generate_df_error(SYSTEM_NO,ls_process_runs)
 # seq.generate_hyperparameter_dataframe(SYSTEM_NO) # OUT DATED
