5270d31988baf06df2b2dcb35ee616beb00d8df5
raghawardhan585 <raghawardhan585@gmail.com>
2020-12-03 02:15:13 -0800

    OT runs ACQUIRED

--- Paper_Plots_Generation.py
+++ Paper_Plots_Generation.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
+import itertools
+import ocdeepdmd_simulation_examples_helper_functions as oc
 
 colors = [[0.68627453, 0.12156863, 0.16470589],
           [0.96862745, 0.84705883, 0.40000001],
@@ -16,38 +18,58 @@ colors = np.asarray(colors);  # defines a color palette
 
 ## Figure 1 - Data of system 1
 
-SYSTEM_NO = 7
-sys_folder_name = '/Users/shara/Box/YeungLabUCSBShare/Shara/DoE_Pputida_RNASeq_DataProcessing/System_' + str(SYSTEM_NO)
+# System Parameters
 
-with open(sys_folder_name + '/System_' + str(SYSTEM_NO) + '_SimulatedData.pickle', 'rb') as handle:
-    dict_data = pickle.load(handle)
+A = np.array([[0.86,0.],[0.8,0.4]])
+gamma = -0.9
+# Simulation Parameters
+N_data_points = 30
+N_CURVES = 240
+sys_params = {'A':A , 'gamma': gamma, 'N_data_points': N_data_points}
+
+dict_data = {}
+X0 = np.empty(shape=(0, 2))
+i=0
+for x1,x2 in itertools.product(list(np.arange(-10,11,2)), list(np.arange(-125,21,20))):
+    sys_params['x0'] = np.array([[x1,x2]])
+    X0 = np.concatenate([X0, sys_params['x0']], axis=0)
+    dict_data[i] = oc.sim_sys_1_2(sys_params)
+    i = i+1
+    # ax[0].plot(dict_indexed_data[i]['X'][:, 0], dict_indexed_data[i]['X'][:, 1])
+    # ax[1].plot(dict_indexed_data[i]['Y'])
+# ax[0].plot(X0[:, 0], X0[:, 1], '*')
+# plt.show()
+
+
+##
 
 plt.figure()
-alpha = 0.5
-epsilon = alpha - 0.1
-arrow_length = 0.01
-ls_pts = list(range(0,20,2))
-for i in list(dict_data.keys())[0:200]:
+alpha = 1.0
+epsilon = alpha - 0.01
+arrow_length = 0.03
+ls_pts = list(range(0,1))
+for i in list(dict_data.keys())[0:]:
     for j in ls_pts:
         if np.abs(dict_data[i]['X'][j, 0]) > 1 or j==0:
-            plt.plot(dict_data[i]['X'][j, 0], dict_data[i]['X'][j, 1], 'o',color='salmon',fillstyle='none',markersize=3)
+            plt.plot(dict_data[i]['X'][j, 0], dict_data[i]['X'][j, 1], 'o',color='salmon',fillstyle='none',markersize=5)
     plt.plot(dict_data[i]['X'][:, 0], dict_data[i]['X'][:, 1], color='tab:blue',linewidth=0.5)
     if np.mod(i,1)==0:
-        for j in ls_pts[0:2]:
+        for j in ls_pts:
             dist = np.sqrt((dict_data[i]['X'][j, 0] - dict_data[i]['X'][j + 1, 0]) ** 2 + (dict_data[i]['X'][j, 1] - dict_data[i]['X'][j + 1, 1]) ** 2)
-            if dist < 2:
-                break
-            x = alpha * dict_data[i]['X'][j, 0] + (1 - alpha) * dict_data[i]['X'][j+1, 0]
-            y = alpha * dict_data[i]['X'][j, 1] + (1 - alpha) * dict_data[i]['X'][j+1, 1]
-            dx = (epsilon * dict_data[i]['X'][j, 0] + (1 - epsilon) * dict_data[i]['X'][j+1, 0] - x)*arrow_length*dist
-            dy = (epsilon * dict_data[i]['X'][j, 1] + (1 - epsilon) * dict_data[i]['X'][j+1, 1] - y)*arrow_length*dist
-            plt.arrow(x,y,dx,dy,head_width = 0.3,head_length=3,alpha=0.5,color='tab:green')
+            x = dict_data[i]['X'][j, 0]
+            y = dict_data[i]['X'][j, 1]
+            dx = (dict_data[i]['X'][j+1, 0] - dict_data[i]['X'][j, 0] )*arrow_length
+            dy = (dict_data[i]['X'][j+1, 1] - dict_data[i]['X'][j, 1] )*arrow_length
+            print(x,' ',y,' ',dx,' ',dy)
+            plt.arrow(x,y,dx,dy,head_width = 0.3,head_length=3,alpha=1,color='tab:green')
 plt.xlabel('x1')
 plt.ylabel('x2')
 plt.plot([0],[0],'o',color='tab:red',markersize=10)
+plt.xlim([-10,10])
+plt.ylim([-125,20])
 plt.show()
 
-plt.figure()
+# plt.figure()
 
 
 ##
