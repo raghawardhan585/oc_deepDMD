f96681a886cc6f1f39b45f02c0828c385b89e9a3
raghawardhan585 <raghawardhan585@gmail.com>
2020-12-04 12:01:05 -0800

    MT runs ACQUIRED

---
+++ SimSys2_Analysis.py
@@ -0,0 +1,26 @@
+##
+import pickle
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import tensorflow as tf
+import ocdeepdmd_simulation_examples_helper_functions as oc
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import shutil
+
+colors = [[0.68627453, 0.12156863, 0.16470589],
+          [0.96862745, 0.84705883, 0.40000001],
+          [0.83137256, 0.53333336, 0.6156863],
+          [0.03529412, 0.01960784, 0.14509805],
+          [0.90980393, 0.59607846, 0.78039217],
+          [0.69803923, 0.87843138, 0.72941178],
+          [0.20784314, 0.81568629, 0.89411765]];
+colors = np.asarray(colors);  # defines a color palette
+
+
+SYSTEM_NO = 7
+sys_folder_name = '/Users/shara/Box/YeungLabUCSBShare/Shara/DoE_Pputida_RNASeq_DataProcessing/System_' + str(SYSTEM_NO)
+
+# Theoretical Eigenfunctions
