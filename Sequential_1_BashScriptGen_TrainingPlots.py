2f245b6a6f6d0c96ee9fe975f094418424cc4cb5
raghawardhan585 <raghawardhan585@gmail.com>
2020-12-07 16:04:39 -0800

    Next runs

--- Sequential_1_BashScriptGen_TrainingPlots.py
+++ Sequential_1_BashScriptGen_TrainingPlots.py
@@ -33,11 +33,11 @@ for DEVICE_TO_RUN_ON in ['microtensor','optictensor','goldentensor']:
     if DEVICE_TO_RUN_ON == 'microtensor':
         # MICROTENSOR CPU RUN
         dict_run_conditions[0] = {}
-        dict_run_conditions[0]['x']  = {'dict_size':17,'nn_layers':4,'nn_nodes':19}
+        dict_run_conditions[0]['x']  = {'dict_size':19,'nn_layers':4,'nn_nodes':31}
         dict_run_conditions[0]['y']  = {'dict_size':1,'nn_layers':4,'nn_nodes':3}
         dict_run_conditions[0]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':3}
         dict_run_conditions[1] = {}
-        dict_run_conditions[1]['x']  = {'dict_size':17,'nn_layers':4,'nn_nodes':24}
+        dict_run_conditions[1]['x']  = {'dict_size':19,'nn_layers':4,'nn_nodes':36}
         dict_run_conditions[1]['y']  = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
         dict_run_conditions[1]['xy'] = {'dict_size':1,'nn_layers':3,'nn_nodes':6}
         # dict_run_conditions[2] = {}
@@ -51,19 +51,19 @@ for DEVICE_TO_RUN_ON in ['microtensor','optictensor','goldentensor']:
     elif DEVICE_TO_RUN_ON =='goldentensor':
         # Golden tensor
         dict_run_conditions[0] = {}
-        dict_run_conditions[0]['x']  = {'dict_size':18,'nn_layers':4,'nn_nodes':20}
+        dict_run_conditions[0]['x']  = {'dict_size':18,'nn_layers':4,'nn_nodes':30}
         dict_run_conditions[0]['y']  = {'dict_size':1,'nn_layers':4,'nn_nodes':3}
         dict_run_conditions[0]['xy'] = {'dict_size':2,'nn_layers':3,'nn_nodes':3}
         dict_run_conditions[1] = {}
-        dict_run_conditions[1]['x']  = {'dict_size':18,'nn_layers':4,'nn_nodes':25}
+        dict_run_conditions[1]['x']  = {'dict_size':18,'nn_layers':4,'nn_nodes':35}
         dict_run_conditions[1]['y']  = {'dict_size':1,'nn_layers':4,'nn_nodes':6}
         dict_run_conditions[1]['xy'] = {'dict_size':2,'nn_layers':3,'nn_nodes':6}
         dict_run_conditions[2] = {}
-        dict_run_conditions[2]['x']  = {'dict_size':19,'nn_layers':4,'nn_nodes':21}
+        dict_run_conditions[2]['x']  = {'dict_size':20,'nn_layers':4,'nn_nodes':32}
         dict_run_conditions[2]['y']  = {'dict_size':1,'nn_layers':4,'nn_nodes':9}
         dict_run_conditions[2]['xy'] = {'dict_size':2,'nn_layers':4,'nn_nodes':3}
         dict_run_conditions[3] = {}
-        dict_run_conditions[3]['x']  = {'dict_size':19,'nn_layers':4,'nn_nodes':26}
+        dict_run_conditions[3]['x']  = {'dict_size':20,'nn_layers':4,'nn_nodes':39}
         dict_run_conditions[3]['y']  = {'dict_size':1,'nn_layers':4,'nn_nodes':12}
         dict_run_conditions[3]['xy'] = {'dict_size':2,'nn_layers':4,'nn_nodes':6}
     elif DEVICE_TO_RUN_ON == 'optictensor':
@@ -98,7 +98,7 @@ seq.generate_df_error(SYSTEM_NO,ls_process_runs)
 
 
 ## RUN 1 - Display hyperparameters of the runs
-SYSTEM_NO = 23
+SYSTEM_NO = 25
 sys_folder_name = '/Users/shara/Box/YeungLabUCSBShare/Shara/DoE_Pputida_RNASeq_DataProcessing/System_' + str(SYSTEM_NO)
 # ls_process_runs = list(range(115,116))
 for run in ls_process_runs:
@@ -340,7 +340,7 @@ plt.show()
 SYSTEM_NO = 25
 ls_process_runs = list(range(0,64))#set(range(0, 120)).union(range(216,260))
 # ls_process_runs = list(range(168, 216))
-ls_steps = list(range(1,20,1))
+ls_steps = list(range(1,20,2))
 sys_folder_name = '/Users/shara/Box/YeungLabUCSBShare/Shara/DoE_Pputida_RNASeq_DataProcessing/System_' + str(SYSTEM_NO)
 with open(sys_folder_name + '/System_' + str(SYSTEM_NO) + '_SimulatedData.pickle','rb') as handle:
     var_i = pickle.load(handle)
