3adb2d09f73ed8a128eda317810a14c0a6964412
raghawardhan585 <raghawardhan585@gmail.com>
2020-12-08 15:58:55 -0800

    Next runs

--- 1_SimulatedSystems_DataGeneration.py
+++ 1_SimulatedSystems_DataGeneration.py
@@ -90,13 +90,15 @@ k_3d = 1.08
 # k_3n = 3.
 # k_3d = 1.08
 
-x_min = np.asarray([0.1,0.3,0.9,0.3])
-x_max = np.asarray([0.2,1.,1.,1.])
+# x_min = np.asarray([0.1,0.3,0.9,0.3])
+# x_max = np.asarray([0.2,1.,1.,1.])
+x_min = np.asarray([0.2,0.7,0.9,0.3])
+x_max = np.asarray([0.2,0.7,0.9,0.3])
 sys_params_arc4s = (gamma_A,gamma_B,delta_A,delta_B,alpha_A0,alpha_B0,alpha_A,alpha_B,K_A,K_B,kappa_A,kappa_B,n,m)
 # Simulation Parameters
-sampling_time = 0.5
+sampling_time = 0.2
 simulation_time = 30
-N_CURVES = 300
+N_CURVES = 3
 
 sys_params = {'sys_params_arc4s': sys_params_arc4s , 'k_3n':k_3n, 'k_3d':k_3d, 'Ts': sampling_time, 't_end': simulation_time,'N_CURVES': N_CURVES, 'x_min': x_min, 'x_max':x_max}
 SYSTEM_NO = 27
--- Sequential_1_BashScriptGen_TrainingPlots.py
+++ Sequential_1_BashScriptGen_TrainingPlots.py
@@ -92,7 +92,7 @@ seq.transfer_current_ocDeepDMD_run_files()
 
 ## RUN 1 PROCESSING - Generate predictions and error
 SYSTEM_NO = 27
-ls_process_runs = list(range(80,90)) # Runs for which we want to calculate the error
+ls_process_runs = list(range(0,24)) # Runs for which we want to calculate the error
 seq.generate_predictions_pickle_file(SYSTEM_NO,state_only =True,ls_process_runs=ls_process_runs)
 seq.generate_df_error(SYSTEM_NO,ls_process_runs)
 
@@ -108,7 +108,7 @@ for run in ls_process_runs:
 
 
 ## RUN 1 PROCESSING - Get the optimal run from the specified runs
-SYSTEM_NO = 31
+SYSTEM_NO = 27
 sys_folder_name = '/Users/shara/Box/YeungLabUCSBShare/Shara/DoE_Pputida_RNASeq_DataProcessing/System_' + str(SYSTEM_NO)
 with open(sys_folder_name + '/df_error_SEQUENTIAL.pickle','rb') as handle:
     df_error = pickle.load(handle)
@@ -136,7 +136,7 @@ del var_i
 ls_train_curves = list(range(int(np.floor(N_CURVES/3))))
 ls_valid_curves = list(range(ls_train_curves[-1] + 1 ,ls_train_curves[-1] + 1 + int(np.floor(N_CURVES/3))))
 ls_test_curves = list(range(ls_valid_curves[-1]+1,N_CURVES))
-f1 = seq.plot_fit_XY(dict_predictions_opt_run,plot_params,ls_train_curves[0:20],scaled=True,observables=True,one_step=False)
+f1 = seq.plot_fit_XY(dict_predictions_opt_run,plot_params,ls_train_curves[0:20],scaled=True,observables=True,one_step=True)
 # f1 = seq.plot_fit_XY(dict_predictions_opt_run,plot_params,ls_train_curves[0:20],scaled=True,observables=True,one_step=True)
 # f1 = seq.plot_fit_XY(dict_predictions_opt_run,plot_params,ls_test_curves[0:20],scaled=False,observables=True,one_step=False)
 
