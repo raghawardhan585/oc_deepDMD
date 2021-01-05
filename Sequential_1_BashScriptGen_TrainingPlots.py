ee5e2f5ce030abe9c3ac1ca82b2e7903b9550c5a
raghawardhan585 <raghawardhan585@gmail.com>
2021-01-05 11:09:06 -0800

    OT runs ACQUIRED

--- Sequential_1_BashScriptGen_TrainingPlots.py
+++ Sequential_1_BashScriptGen_TrainingPlots.py
@@ -207,8 +207,8 @@ for items in ls_files:
 seq.transfer_current_ocDeepDMD_run_files()
 
 ## RUN 1 PROCESSING - Generate predictions and error
-SYSTEM_NO = 53
-ls_process_runs = list(range(0,71)) # Runs for which we want to calculate the error
+SYSTEM_NO = 10
+ls_process_runs = list(range(0,45)) # Runs for which we want to calculate the error
 # ls_process_runs = list(range(84,85))
 seq.generate_predictions_pickle_file(SYSTEM_NO,state_only =True,ls_process_runs=ls_process_runs)
 seq.generate_df_error(SYSTEM_NO,ls_process_runs)
