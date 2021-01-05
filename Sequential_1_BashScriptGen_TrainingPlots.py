bf239647f757f46d3bf5e9b5eba17841d0519a09
raghawardhan585 <raghawardhan585@gmail.com>
2021-01-05 15:17:57 -0800

    Next runs

--- Sequential_1_BashScriptGen_TrainingPlots.py
+++ Sequential_1_BashScriptGen_TrainingPlots.py
@@ -35,14 +35,14 @@ dict_hp['x']['ls_dict_size'] = [2]
 dict_hp['x']['ls_nn_layers'] = [3]
 dict_hp['x']['ls_nn_nodes'] = [8]
 dict_hp['y']={}
-dict_hp['y']['ls_dict_size'] = [1,2,3]
-dict_hp['y']['ls_nn_layers'] = [3,4,5]
-dict_hp['y']['ls_nn_nodes'] = [3,6,9]
+dict_hp['y']['ls_dict_size'] = [5]
+dict_hp['y']['ls_nn_layers'] = [9]
+dict_hp['y']['ls_nn_nodes'] = [1]
 dict_hp['xy']={}
-dict_hp['xy']['ls_dict_size'] = [2,3,4]
-dict_hp['xy']['ls_nn_layers'] = [4,5,6,7,8]
-dict_hp['xy']['ls_nn_nodes'] = [5,10,15,20,25]
-process_variable = 'y'
+dict_hp['xy']['ls_dict_size'] = [1,2,3]
+dict_hp['xy']['ls_nn_layers'] = [3,4,5]
+dict_hp['xy']['ls_nn_nodes'] = [3,4,5]
+process_variable = 'xy'
 SYSTEM_NO = DATA_SYSTEM_TO_WRITE_BASH_SCRIPT_FOR
 
 ls_dict_size = dict_hp[process_variable]['ls_dict_size']
@@ -271,7 +271,7 @@ print(dict_hp)
 
 ## RUN 2 - Training error plot [USELESS UNLESS DEBUGGING]
 SYSTEM_NO = 10
-ls_run_no = list(range(45,51))
+ls_run_no = list(range(45,72))
 plot_params ={}
 plot_params['xy_label_font_size']=9
 plot_params['individual_fig_width']=2
@@ -288,7 +288,7 @@ ls_train_curves = list(range(int(np.floor(N_CURVES/3))))
 ls_valid_curves = list(range(ls_train_curves[-1] + 1 ,ls_train_curves[-1] + 1 + int(np.floor(N_CURVES/3))))
 ls_test_curves = list(range(ls_valid_curves[-1]+1,N_CURVES))
 
-ls_process_run_indices = list(range(45,51))
+ls_process_run_indices = list(range(45,72))
 seq.generate_predictions_pickle_file_output_only(SYSTEM_NO,ls_process_run_indices)
 seq.generate_df_error_output(SYSTEM_NO)
 with open(sys_folder_name + '/df_error_SEQUENTIAL_OUTPUT.pickle','rb') as handle:
--- optictensor_run.sh
+++ optictensor_run.sh
@@ -4,20 +4,20 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [x_dict] [x_layers] [x_nodes] [y_dict] [y_layers] [y_nodes] [xy_dict] [xy_layers] [xy_nodes] [write_to_file] 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 10 0  1 1 1 1 3 9 1 1 1 > Run_info/SYS_10_RUN_0.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 10 1  1 1 1 1 4 3 1 1 1 > Run_info/SYS_10_RUN_1.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 10 2  1 1 1 1 4 6 1 1 1 > Run_info/SYS_10_RUN_2.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 10 3  1 1 1 1 4 9 1 1 1 > Run_info/SYS_10_RUN_3.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:0' 10 0  1 1 1 1 1 1 1 3 5 > Run_info/SYS_10_RUN_0.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:1' 10 1  1 1 1 1 1 1 1 4 3 > Run_info/SYS_10_RUN_1.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:2' 10 2  1 1 1 1 1 1 1 4 4 > Run_info/SYS_10_RUN_2.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:3' 10 3  1 1 1 1 1 1 1 4 5 > Run_info/SYS_10_RUN_3.txt &
 wait 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 10 4  1 1 1 2 4 3 1 1 1 > Run_info/SYS_10_RUN_4.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 10 5  1 1 1 2 4 6 1 1 1 > Run_info/SYS_10_RUN_5.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 10 6  1 1 1 2 4 9 1 1 1 > Run_info/SYS_10_RUN_6.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 10 7  1 1 1 2 5 3 1 1 1 > Run_info/SYS_10_RUN_7.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:0' 10 4  1 1 1 1 1 1 2 4 3 > Run_info/SYS_10_RUN_4.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:1' 10 5  1 1 1 1 1 1 2 4 4 > Run_info/SYS_10_RUN_5.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:2' 10 6  1 1 1 1 1 1 2 4 5 > Run_info/SYS_10_RUN_6.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:3' 10 7  1 1 1 1 1 1 2 5 3 > Run_info/SYS_10_RUN_7.txt &
 wait 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 10 8  1 1 1 3 4 6 1 1 1 > Run_info/SYS_10_RUN_8.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 10 9  1 1 1 3 4 9 1 1 1 > Run_info/SYS_10_RUN_9.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 10 10  1 1 1 3 5 3 1 1 1 > Run_info/SYS_10_RUN_10.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 10 11  1 1 1 3 5 6 1 1 1 > Run_info/SYS_10_RUN_11.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:0' 10 8  1 1 1 1 1 1 3 4 4 > Run_info/SYS_10_RUN_8.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:1' 10 9  1 1 1 1 1 1 3 4 5 > Run_info/SYS_10_RUN_9.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:2' 10 10  1 1 1 1 1 1 3 5 3 > Run_info/SYS_10_RUN_10.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:3' 10 11  1 1 1 1 1 1 3 5 4 > Run_info/SYS_10_RUN_11.txt &
 wait 
 wait 
 echo "All sessions are complete" 
