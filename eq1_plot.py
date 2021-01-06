fbc466076f3af6f16848e7915f2292fe66b73efa
raghawardhan585 <raghawardhan585@gmail.com>
2021-01-05 19:34:52 -0800

    MT runs ACQUIRED

--- eq1_plot.py
+++ eq1_plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
 import pickle
-
+import tensorflow as tf
 
 SYS_NO = 10
 RUN_NO = 0
@@ -30,3 +30,37 @@ plt.show()
 
 ## Getting the Koopman Modes
 
+sess = tf.InteractiveSession()
+saver = tf.compat.v1.train.import_meta_graph(run_folder_name + '/System_' + str(SYS_NO) + '_ocDeepDMDdata.pickle.ckpt.meta', clear_devices=True)
+saver.restore(sess, tf.train.latest_checkpoint(run_folder_name))
+dict_params = {}
+try:
+    psixpT = tf.get_collection('psixpT')[0]
+    psixfT = tf.get_collection('psixfT')[0]
+    xpT_feed = tf.get_collection('xpT_feed')[0]
+    xfT_feed = tf.get_collection('xfT_feed')[0]
+    KxT = tf.get_collection('KxT')[0]
+    KxT_num = sess.run(KxT)
+    dict_params['psixpT'] = psixpT
+    dict_params['psixfT'] = psixfT
+    dict_params['xpT_feed'] = xpT_feed
+    dict_params['xfT_feed'] = xfT_feed
+    dict_params['KxT_num'] = KxT_num
+except:
+    print('State info not found')
+try:
+    ypT_feed = tf.get_collection('ypT_feed')[0]
+    yfT_feed = tf.get_collection('yfT_feed')[0]
+    dict_params['ypT_feed'] = ypT_feed
+    dict_params['yfT_feed'] = yfT_feed
+    WhT = tf.get_collection('WhT')[0];
+    WhT_num = sess.run(WhT)
+    dict_params['WhT_num'] = WhT_num
+except:
+    print('No output info found')
+
+eval,evec = np.linalg.eig(dict_params['KxT_num'])
+kmode = np.empty(shape=(len(eval),0))
+N_steps = 100
+for i in range(N_steps):
+    k
