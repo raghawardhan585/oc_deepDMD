a4ee2ccb8e9adbf6a114feef81cbdf64f180730c
raghawardhan585 <raghawardhan585@gmail.com>
2021-01-10 21:31:35 -0800

    Next runs

--- optictensor_run.sh
+++ optictensor_run.sh
@@ -4,10 +4,10 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [process var] [run_no] [n_layers] [n_nodes] [write_to_file] 
-python3 hammerstein_nn_identification.py '/gpu:0' 60 'x' 0 3 15 > Run_info/SYS_60_RUN_0.txt &
-python3 hammerstein_nn_identification.py '/gpu:1' 60 'x' 1 4 5 > Run_info/SYS_60_RUN_1.txt &
-python3 hammerstein_nn_identification.py '/gpu:2' 60 'x' 2 4 10 > Run_info/SYS_60_RUN_2.txt &
-python3 hammerstein_nn_identification.py '/gpu:3' 60 'x' 3 4 15 > Run_info/SYS_60_RUN_3.txt &
+python3 hammerstein_nn_identification.py '/gpu:0' 53 'x' 0 3 25 > Run_info/SYS_53_RUN_0.txt &
+python3 hammerstein_nn_identification.py '/gpu:1' 53 'x' 1 4 15 > Run_info/SYS_53_RUN_1.txt &
+python3 hammerstein_nn_identification.py '/gpu:2' 53 'x' 2 4 20 > Run_info/SYS_53_RUN_2.txt &
+python3 hammerstein_nn_identification.py '/gpu:3' 53 'x' 3 4 25 > Run_info/SYS_53_RUN_3.txt &
 wait 
 wait 
 echo "All sessions are complete" 
