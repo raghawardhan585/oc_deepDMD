ed0acf5c377c65c5e8545f6f255e73984a186ac8
raghawardhan585 <raghawardhan585@gmail.com>
2021-01-10 23:04:55 -0800

    Next runs

--- goldentensor_run.sh
+++ goldentensor_run.sh
@@ -4,9 +4,9 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [process var] [run_no] [n_layers] [n_nodes] [write_to_file] 
-python3 hammerstein_nn_identification.py '/gpu:0' 53 'x' 0 5 15 > Run_info/SYS_53_RUN_0.txt &
-python3 hammerstein_nn_identification.py '/gpu:1' 53 'x' 1 5 20 > Run_info/SYS_53_RUN_1.txt &
-python3 hammerstein_nn_identification.py '/gpu:2' 53 'x' 2 5 25 > Run_info/SYS_53_RUN_2.txt &
+python3 hammerstein_nn_identification.py '/gpu:0' 60 'x' 0 5 15 > Run_info/SYS_60_RUN_0.txt &
+python3 hammerstein_nn_identification.py '/gpu:1' 60 'x' 1 5 20 > Run_info/SYS_60_RUN_1.txt &
+python3 hammerstein_nn_identification.py '/gpu:2' 60 'x' 2 5 25 > Run_info/SYS_60_RUN_2.txt &
 wait 
 echo "All sessions are complete" 
 echo "=======================================================" 
--- hammerstein_nn_identification.py
+++ hammerstein_nn_identification.py
@@ -52,7 +52,7 @@ dict_training_params = {'step_size_val': 00.5, 'train_error_threshold': float(1e
 ls_dict_training_params.append(dict_training_params)
 dict_training_params = {'step_size_val': 00.3, 'train_error_threshold': float(1e-6),'valid_error_threshold': float(1e-6), 'max_epochs': 10000, 'batch_size': 1000}
 ls_dict_training_params.append(dict_training_params)
-dict_training_params = {'step_size_val': 0.1, 'train_error_threshold': float(1e-7), 'valid_error_threshold': float(1e-7), 'max_epochs': 20000, 'batch_size': 1000}
+dict_training_params = {'step_size_val': 0.1, 'train_error_threshold': float(1e-7), 'valid_error_threshold': float(1e-7), 'max_epochs': 50000, 'batch_size': 1000}
 ls_dict_training_params.append(dict_training_params)
 # dict_training_params = {'step_size_val': 0.09, 'train_error_threshold': float(1e-8), 'valid_error_threshold': float(1e-8), 'max_epochs': 10000, 'batch_size': 500}
 # ls_dict_training_params.append(dict_training_params)
