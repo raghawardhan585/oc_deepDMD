6a768492651c4d895f0b81810a9873250e135205
raghawardhan585 <raghawardhan585@gmail.com>
2021-01-10 16:22:19 -0800

    Next runs

--- goldentensor_run.sh
+++ goldentensor_run.sh
@@ -4,9 +4,6 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [process var] [run_no] [n_layers] [n_nodes] [write_to_file] 
-python3 hammerstein_nn_identification.py '/gpu:0' 153 'x' 0 5 15 > Run_info/SYS_153_RUN_0.txt &
-python3 hammerstein_nn_identification.py '/gpu:1' 153 'x' 1 5 20 > Run_info/SYS_153_RUN_1.txt &
-python3 hammerstein_nn_identification.py '/gpu:2' 153 'x' 2 5 25 > Run_info/SYS_153_RUN_2.txt &
 wait 
 echo "All sessions are complete" 
 echo "=======================================================" 
