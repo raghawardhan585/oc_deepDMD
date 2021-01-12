a8444fc7a43fe9f47ad845072078c833c6f8f712
raghawardhan585 <raghawardhan585@gmail.com>
2021-01-12 14:46:53 -0800

    MT runs ACQUIRED

--- eg4_plot_glycolytic_oscillator.py
+++ eg4_plot_glycolytic_oscillator.py
@@ -31,7 +31,6 @@ SYS_NO = 53
 RUN_NO = 344
 RUN_NO_HAMMERSTEIN = 0
 RUN_NO_DEEPDMD = 22
-REDUCE_MODES = False
 
 sys_folder_name = '/Users/shara/Box/YeungLabUCSBShare/Shara/DoE_Pputida_RNASeq_DataProcessing/System_' + str(SYS_NO)
 run_folder_name = sys_folder_name + '/Sequential/RUN_' + str(RUN_NO)
@@ -46,17 +45,18 @@ for items in dict_oc_data:
     dict_oc_data[items] = dict_oc_data[items][0:Ntrain]
 with open(sys_folder_name + '/dict_predictions_SEQUENTIAL.pickle', 'rb') as handle:
     d_SEQ = pickle.load(handle)[RUN_NO]
-with open(sys_folder_name + '/dict_predictions_Hammerstein.pickle', 'rb') as handle:
-    d_HAM = pickle.load(handle)[RUN_NO_HAMMERSTEIN]
+# with open(sys_folder_name + '/dict_predictions_Hammerstein.pickle', 'rb') as handle:
+#     d_HAM = pickle.load(handle)[RUN_NO_HAMMERSTEIN]
 with open(sys_folder_name + '/dict_predictions_deepDMD.pickle', 'rb') as handle:
     d_DDMD = pickle.load(handle)[RUN_NO_DEEPDMD]
-#
 
 ##
 ls_steps = list(range(1,50,1))
 ls_curves = list(range(200, 300)) # test curves
+Senergy_THRESHOLD = 99.99
+REDUCED_MODES = False
+RIGHT_EIGEN_VECTORS = True
 
-##
 
 def get_dict_param(run_folder_name_curr,SYS_NO,sess):
     dict_p = {}
@@ -116,38 +116,51 @@ def r2_n_step_prediction_accuracy(ls_steps,ls_curves,dict_data,dict_params_curr)
             OPT_CURVE_NO = i
             break
     return df_r2, OPT_CURVE_NO
-
-dict_params = {}
-
-
-sess1 = tf.InteractiveSession()
-dict_params['Seq'] = get_dict_param(run_folder_name,SYS_NO,sess1)
-df_r2_SEQ, CURVE_NO = r2_n_step_prediction_accuracy(ls_steps,ls_curves,dict_data,dict_params['Seq'])
-tf.reset_default_graph()
-sess1.close()
-
-
-
-
-sess2 = tf.InteractiveSession()
-dict_params['Deep'] = get_dict_param(run_folder_name_DEEPDMD,SYS_NO,sess2)
-df_r2_DEEPDMD, _ = r2_n_step_prediction_accuracy(ls_steps,ls_curves,dict_data,dict_params['Deep'])
-tf.reset_default_graph()
-sess2.close()
-
-
-
-
-## Dynamic Modes
-
-Senergy_THRESHOLD = 99.9
-
-def eig_func_through_time(dict_oc_data,dict_params_curr,REDUCED_MODES):
+def resolve_complex_right_eigenvalues(E, W):
+    eval = np.diag(E)
+    comp_modes = []
+    comp_modes_conj = []
+    for i1 in range(E.shape[0]):
+        if np.imag(E[i1, i1]) != 0:
+            print(i1)
+            # Find the complex conjugate
+            for i2 in range(i1 + 1, E.shape[0]):
+                if eval[i2] == eval[i1].conj():
+                    break
+            # i1 and i2 are the indices of the complex conjugate eigenvalues
+            comp_modes.append(i1)
+            comp_modes_conj.append(i2)
+            E[i1, i1] = np.real(eval[i1])
+            E[i2, i2] = np.real(eval[i1])
+            E[i1, i2] = np.imag(eval[i1])
+            E[i2, i1] = - np.imag(eval[i1])
+            u1 = copy.deepcopy(np.real(W[:, i1:i1 + 1]))
+            w1 = copy.deepcopy(np.imag(W[:, i1:i1 + 1]))
+            W[:, i1:i1 + 1] = u1
+            W[:, i2:i2 + 1] = w1
+    E_out = np.real(E)
+    W_out = np.real(W)
+    return E_out, W_out, comp_modes, comp_modes_conj
+def eig_func_through_time(dict_oc_data,dict_data_curr,dict_params_curr,REDUCED_MODES,Senergy_THRESHOLD,RIGHT_EIGEN_VECTORS = True,SHOW_PCA_X=True):
     psiX = dict_params_curr['psixpT'].eval(feed_dict={dict_params_curr['xpT_feed']: dict_oc_data['Xp']}).T
+    Phi0 = dict_params_curr['psixpT'].eval(feed_dict={dict_params_curr['xpT_feed']: dict_data_curr['X'][0:1]})
+    if SHOW_PCA_X:
+        _, s, _T = np.linalg.svd(dict_oc_data['Xp'])
+        plt.stem(np.arange(len(s)), (np.cumsum(s ** 2) / np.sum(s ** 2)) * 100)
+        plt.plot([0, len(s) - 1], [100, 100])
+        plt.title('just X')
+        plt.show()
+        plt.figure()
+        _, s, _T = np.linalg.svd(psiX)
+        plt.stem(np.arange(len(s)), (np.cumsum(s ** 2) / np.sum(s ** 2)) * 100)
+        plt.plot([0, len(s) - 1], [100, 100])
+        plt.title('psiX')
+        plt.show()
+    #
     K = dict_params_curr['KxT_num'].T
     if REDUCED_MODES:
         # Minimal POD modes of psiX
-        U, S, VT = np.linalg.svd(psiXp_data)
+        U, S, VT = np.linalg.svd(psiX)
         Senergy = np.cumsum(S ** 2) / np.sum(S ** 2) * 100
         for i in range(len(S)):
             if Senergy[i] > Senergy_THRESHOLD:
@@ -156,106 +169,73 @@ def eig_func_through_time(dict_oc_data,dict_params_curr,REDUCED_MODES):
         print('Optimal POD modes chosen : ', nPC)
         Ur = U[:, 0:nPC]
         Kred = np.matmul(np.matmul(Ur.T, K), Ur)
+        if RIGHT_EIGEN_VECTORS:
+            eval, W = np.linalg.eig(Kred)
+            E = np.diag(eval)
+            E,W, comp_modes, comp_modes_conj = resolve_complex_right_eigenvalues(E,W)
+            Winv = np.linalg.inv(W)
+            Phi = np.matmul(E, np.matmul(np.matmul(Winv, Ur.T), Phi0.T))
+            for i in range(1,len(Phi0)):
+                Phi = np.concatenate([Phi,np.matmul(E,Phi[:,-1:])],axis=1)
+            koop_modes = W
+        else:
+            #TODO - Do what happens when left eigenvectors are inserted here
+            print('Meh')
+    else:
+        if RIGHT_EIGEN_VECTORS:
+            eval, W = np.linalg.eig(K)
+            E = np.diag(eval)
+            E, W, comp_modes, comp_modes_conj = resolve_complex_right_eigenvalues(E, W)
+            Winv = np.linalg.inv(W)
+            Phi = np.matmul(E, np.matmul(Winv,Phi0.T))
+            for i in range(1, len(Phi0)):
+                Phi = np.concatenate([Phi, np.matmul(E, Phi[:, -1:])], axis=1)
+            koop_modes = W
+        else:
+            #TODO - Do what happens when left eigenvectors are inserted here
+            print('Meh')
+    return Phi, koop_modes, comp_modes, comp_modes_conj
 
-    return
 
-# Required Variables - K, psiX,
-CURVE_NO = 0
-psiX = d_SEQ[CURVE_NO]['psiX'].T
-K = dict_params['KxT_num'].T
-psiXp_data = psiX[:,0:-1]
-psiXf_data = psiX[:,1:]
-if REDUCE_MODES:
-    # Minimal POD modes of psiX
-    U,S,VT = np.linalg.svd(psiXp_data)
-    Senergy = np.cumsum(S**2)/np.sum(S**2)*100
-    for i in range(len(S)):
-        if Senergy[i] > Senergy_THRESHOLD:
-            nPC = i+1
-            break
-    print('Optimal POD modes chosen : ', nPC)
-    Ur = U[:,0:nPC]
-    plt.figure()
-    _,s,_T = np.linalg.svd(d_SEQ[CURVE_NO]['X'])
-    plt.stem(np.arange(len(s)),(np.cumsum(s**2)/np.sum(s**2))*100)
-    plt.plot([0,len(s)-1],[100,100])
-    plt.title('just X')
-    plt.show()
-    plt.figure()
-    _,s,_T = np.linalg.svd(d_SEQ[CURVE_NO]['psiX'])
-    plt.stem(np.arange(len(s)),(np.cumsum(s**2)/np.sum(s**2))*100)
-    plt.plot([0,len(s)-1],[100,100])
-    plt.title('psiX')
-    plt.show()
-    # Reduced K - Kred
-    Kred = np.matmul(np.matmul(Ur.T,K),Ur)
-else:
-    Kred = K
-    nPC = K.shape[0]
-# Eigendecomposition of Kred - Right eigenvectors
-# eval,W = np.linalg.eig(Kred.T)
-eval,W = np.linalg.eig(Kred)
-E = np.diag(eval)
-comp_modes =[]
-comp_modes_conj =[]
-for i1 in range(len(eval)):
-    if np.imag(E[i1,i1]) != 0:
-        print(i1)
-        # Find the complex conjugate
-        for i2 in range(i1+1,len(eval)):
-            if eval[i2] == eval[i1].conj():
-                break
-        # i1 and i2 are the indices of the complex conjugate eigenvalues
-        comp_modes.append(i1)
-        comp_modes_conj.append(i2)
-        E[i1, i1] = np.real(eval[i1])
-        E[i2, i2] = np.real(eval[i1])
-        E[i1, i2] = np.imag(eval[i1])
-        E[i2, i1] = - np.imag(eval[i1])
-        u1 = copy.deepcopy(np.real(W[:, i1:i1 + 1]))
-        w1 = copy.deepcopy(np.imag(W[:, i1:i1 + 1]))
-        W[:, i1:i1 + 1] = u1
-        W[:, i2:i2 + 1] = w1
-E = np.real(E)
-W = np.real(W)
-Winv = np.linalg.inv(W)
-# Koopman eigenfunctions
-if REDUCE_MODES:
-    Phi = np.matmul(E,np.matmul(np.matmul(Winv,Ur.T),psiXp_data))
-else:
-    Phi = np.matmul(E, np.matmul(Winv, psiXp_data))
-plt.figure()
-plt.plot(Phi.T)
-plt.legend(np.arange(nPC))
+dict_params = {}
+sess1 = tf.InteractiveSession()
+dict_params['Seq'] = get_dict_param(run_folder_name,SYS_NO,sess1)
+df_r2_SEQ, CURVE_NO = r2_n_step_prediction_accuracy(ls_steps,ls_curves,dict_data,dict_params['Seq'])
+Phi_SEQ,koop_modes_SEQ, comp_modes_SEQ, comp_modes_conj_SEQ = eig_func_through_time(dict_oc_data,dict_data[CURVE_NO],dict_params['Seq'],REDUCED_MODES = True,Senergy_THRESHOLD = 99.99,RIGHT_EIGEN_VECTORS=True,SHOW_PCA_X = False)
+tf.reset_default_graph()
+sess1.close()
+
+
+
+#
+sess2 = tf.InteractiveSession()
+dict_params['Deep'] = get_dict_param(run_folder_name_DEEPDMD,SYS_NO,sess2)
+df_r2_DEEPDMD, _ = r2_n_step_prediction_accuracy(ls_steps,ls_curves,dict_data,dict_params['Deep'])
+Phi_DEEPDMD,koop_modes_DEEPDMD, comp_modes_DEEPDMD, comp_modes_conj_DEEPDMD = eig_func_through_time(dict_oc_data,dict_data[CURVE_NO],dict_params['Deep'],REDUCED_MODES = False,Senergy_THRESHOLD = 99.99,RIGHT_EIGEN_VECTORS=True,SHOW_PCA_X = False)
+tf.reset_default_graph()
+sess2.close()
+
+
+
+
+## Dynamic Modes
+
+
+# plt.figure()
+# plt.plot(Phi.T)
+# plt.legend(np.arange(nPC))
+# plt.show()
+sb.heatmap(koop_modes_SEQ,cmap = "YlOrBr",vmin=0.0)
 plt.show()
-
-# Koopman modes - UW
-# Dynamic modes - Lambda*inv(W)*U.T*psiX
-
-
-# sb.heatmap(W,cmap = "YlOrBr",vmin=0.0)
-# plt.show()
-# sb.color_palette("YlOrBr")
-# sb.color_palette("YlOrBr", as_cmap=True)
-# plt.show()
-
-## [R2 function of prediction steps] Calculate the accuracy as a function of the number of steps predicted
-
-# dict_p['psixpT'] = psixpT
-# dict_p['psixfT'] = psixfT
-# dict_p['xpT_feed'] = xpT_feed
-# dict_p['xfT_feed'] = xfT_feed
-# dict_p['KxT_num'] = KxT_num
-# dict_p['ypT_feed'] = ypT_feed
-# dict_p['yfT_feed'] = yfT_feed
-# dict_p['WhT_num'] = WhT_num
+sb.heatmap(koop_modes_DEEPDMD,cmap = "YlOrBr",vmin=0.0)
+plt.show()
 
 
 ## Figure 1 - 1 step prediction comparisons
 
 
-plt.figure(figsize=(20,6))
-plt.subplot2grid((6,20), (0,0), colspan=6, rowspan=6)
+plt.figure(figsize=(10,10))
+plt.subplot2grid((2,2), (0,0), colspan=1, rowspan=1)
 
 
 # CURVE_NO = 0
@@ -277,7 +257,7 @@ for i in range(n_outputs):
     plt.plot([], color=colors[n_states+i], label=('$y_{}$').format(i + 1) + ('$[x10^{{{}}}]$').format(np.int(np.log10(y_scale))))
     plt.plot(d_SEQ[CURVE_NO]['Y'][:,i]/y_scale, '.',color = colors[n_states+i],linewidth = 5)
     plt.plot(d_SEQ[CURVE_NO]['Y_est_one_step'][:, i]/y_scale, linestyle = 'dashed', color=colors[n_states+i])
-    plt.plot(d_DDMD[CURVE_NO]['Y_one_step'][:, i] / y_scale, linestyle='solid', color=colors[i])
+    plt.plot(d_DDMD[CURVE_NO]['Y_one_step'][:, i] / y_scale, linestyle='solid', color=colors[n_states+i])
     # plt.plot(d_HAM[CURVE_NO]['Y_one_step'][:, i] / y_scale, linestyle='solid', color=colors[i])
     pl_max = np.max([pl_max, np.max(d_SEQ[CURVE_NO]['Y'][:, i] / y_scale)])
     pl_min = np.min([pl_min, np.min(d_SEQ[CURVE_NO]['Y'][:, i] / y_scale)])
@@ -292,14 +272,11 @@ plt.xlabel('Time Index(k)')
 plt.ylabel('States and Outputs')
 plt.title('(a)')
 plt.ylim([pl_min,pl_max])
-# plt.show()
+
+
 
 # Figure 2 - n -step prediction comparisons
-
-plt.subplot2grid((6,20), (0,7), colspan=6, rowspan=6)
-
-
-# CURVE_NO = 0
+plt.subplot2grid((2,2), (1,0), colspan=1, rowspan=1)
 n_states = d_SEQ[CURVE_NO]['X'].shape[1]
 n_outputs = d_SEQ[CURVE_NO]['Y'].shape[1]
 pl_max = 0
@@ -318,7 +295,7 @@ for i in range(n_outputs):
     plt.plot([], color=colors[n_states+i], label=('$y_{}$').format(i + 1) + ('$[x10^{{{}}}]$').format(np.int(np.log10(y_scale))))
     plt.plot(d_SEQ[CURVE_NO]['Y'][:,i]/y_scale, '.',color = colors[n_states+i],linewidth = 5)
     plt.plot(d_SEQ[CURVE_NO]['Y_est_n_step'][:, i]/y_scale, linestyle = 'dashed', color=colors[n_states+i])
-    plt.plot(d_DDMD[CURVE_NO]['Y_n_step'][:, i] / y_scale, linestyle='solid', color=colors[i])
+    plt.plot(d_DDMD[CURVE_NO]['Y_n_step'][:, i] / y_scale, linestyle='solid', color=colors[n_states+i])
     # plt.plot(d_HAM[CURVE_NO]['Y_one_step'][:, i] / y_scale, linestyle='solid', color=colors[i])
     pl_max = np.max([pl_max, np.max(d_SEQ[CURVE_NO]['Y'][:, i] / y_scale)])
     pl_min = np.min([pl_min, np.min(d_SEQ[CURVE_NO]['Y'][:, i] / y_scale)])
@@ -331,38 +308,50 @@ a4, = plt.plot([], linestyle ='dashdot',linewidth = 1,label='Hammerstein model',
 l2 = plt.legend((a1,a2,a3),('Truth','Sequential oc-deepDMD','direct oc-deepDMD','Hammerstein model'),loc = "lower right")
 plt.xlabel('Time Index(k)')
 plt.ylabel('States and Outputs')
-plt.title('(a)')
+plt.title('(b)')
 plt.ylim([pl_min,pl_max])
-plt.show()
+# plt.show()
 
-
-##
-plt.subplot2grid((6,20), (0,7), colspan=6, rowspan=6)
-plt.bar(df_r2.index,df_r2.mean(axis=1))
+plt.subplot2grid((2,2), (0,1), colspan=1, rowspan=1)
+plt.bar(df_r2_SEQ.index,df_r2_SEQ.mean(axis=1),color = colors[1],label='Sequential oc-deepDMD')
+plt.plot(df_r2_DEEPDMD.index,df_r2_DEEPDMD.mean(axis=1),color = colors[0],label='direct oc-deepDMD')
+# plt.plot(df_r2_HAM.index,df_r2_HAM.mean(axis=1),color = colors[2],label='Hammerstein model')
 plt.xlim([0.5,50.5])
 plt.ylim([80,100])
 STEPS = 10
+plt.legend()
 plt.xticks(ticks=np.arange(10, 51, step=STEPS),labels=range(10,51,STEPS))
 plt.xlabel('# Prediction Steps')
 plt.ylabel('$r^2$(in %)')
-plt.title('(b)')
+plt.title('(c)')
 
+# plt.subplot2grid((2,2), (1,1), colspan=1, rowspan=1)
+# for i in range(Phi_SEQ.shape[0]):
+#     if i in comp_modes_conj_SEQ:
+#         continue
+#     elif i in comp_modes_SEQ:
+#         # plt.plot(Phi[i, :],label = 'lala')
+#         plt.plot(Phi_SEQ[i,:],label='$\phi_{{{},{}}}(x)$'.format(i+1,comp_modes_conj_SEQ[comp_modes_SEQ.index(i)]+1))
+#     else:
+#         plt.plot(Phi_SEQ[i, :], label='$\phi_{}(x)$'.format(i + 1))
+# plt.legend()
+# plt.xlabel('Time Index(k)')
+# plt.ylabel('Evolution of eigenfunctions')
+# plt.title('(d)')
 
-
-##
-plt.subplot2grid((6,20), (0,14), colspan=6, rowspan=6)
-for i in range(nPC):
-    if i in comp_modes_conj:
+plt.subplot2grid((2,2), (1,1), colspan=1, rowspan=1)
+for i in range(Phi_DEEPDMD.shape[0]):
+    if i in comp_modes_conj_DEEPDMD:
         continue
-    elif i in comp_modes:
+    elif i in comp_modes_DEEPDMD:
         # plt.plot(Phi[i, :],label = 'lala')
-        plt.plot(Phi[i,:],label='$\phi_{{{},{}}}(x)$'.format(i+1,comp_modes_conj[comp_modes.index(i)]+1))
+        plt.plot(Phi_DEEPDMD[i,:],label='$\phi_{{{},{}}}(x)$'.format(i+1,comp_modes_conj_DEEPDMD[comp_modes_DEEPDMD.index(i)]+1))
     else:
-        plt.plot(Phi[i, :], label='$\phi_{}(x)$'.format(i + 1))
+        plt.plot(Phi_DEEPDMD[i, :], label='$\phi_{}(x)$'.format(i + 1))
 plt.legend()
 plt.xlabel('Time Index(k)')
-plt.ylabel('Evolution of eigenfunctions')
-plt.title('(c)')
+plt.ylabel('Evolution of eigenfunctions of deeepDMD')
+plt.title('(d)')
 
 
 
