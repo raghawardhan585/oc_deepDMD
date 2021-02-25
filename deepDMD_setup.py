675ab21c9294712165ce704099b7db34b64dccfa
raghawardhan585 <raghawardhan585@gmail.com>
2021-02-25 12:19:18 -0800

    GT runs ACQUIRED

--- deepDMD_setup.py
+++ deepDMD_setup.py
@@ -27,14 +27,14 @@ colors = np.asarray(colors);  # defines a color palette
 
 
 ## Bash Script Generation
-DATA_SYSTEM_TO_WRITE_BASH_SCRIPT_FOR = 80
+DATA_SYSTEM_TO_WRITE_BASH_SCRIPT_FOR = 11
 NO_OF_ITERATIONS_PER_GPU = 1
 NO_OF_ITERATIONS_IN_CPU = 1
 
 dict_hp={}
-dict_hp['ls_dict_size'] = [7,8,9]
-dict_hp['ls_nn_layers'] = [8,9,10]
-dict_hp['ls_nn_nodes'] = [6,9,12]
+dict_hp['ls_dict_size'] = [1,1,1]
+dict_hp['ls_nn_layers'] = [7,8,9]
+dict_hp['ls_nn_nodes'] = [2,3]
 SYSTEM_NO = DATA_SYSTEM_TO_WRITE_BASH_SCRIPT_FOR
 
 ls_dict_size = dict_hp['ls_dict_size']
