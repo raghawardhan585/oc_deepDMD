0e3ab60d6fa7469d377ce13a76cac6c4b5eed918
raghawardhan585 <raghawardhan585@gmail.com>
2021-02-28 23:50:46 -0800

    Next runs

--- Sequential_1_BashScriptGen_TrainingPlots.py
+++ Sequential_1_BashScriptGen_TrainingPlots.py
@@ -33,9 +33,9 @@ NO_OF_ITERATIONS_IN_CPU = 2
 
 dict_hp={}
 dict_hp['x']={}
-dict_hp['x']['ls_dict_size'] = [3,3,4,4,4]
-dict_hp['x']['ls_nn_layers'] = [8,9]
-dict_hp['x']['ls_nn_nodes'] = [6,8]
+dict_hp['x']['ls_dict_size'] = [3,3,3,3,3]
+dict_hp['x']['ls_nn_layers'] = [8,8]
+dict_hp['x']['ls_nn_nodes'] = [6]
 dict_hp['y']={}
 dict_hp['y']['ls_dict_size'] = [1,1,1]
 dict_hp['y']['ls_nn_layers'] = [7,8,9]
@@ -164,7 +164,7 @@ seq.transfer_current_ocDeepDMD_run_files()
 # SYSTEM_NO = 80
 # ls_process_runs = list(range(0,106)) # Runs for which we want to calculate the error
 SYSTEM_NO = 90
-ls_process_runs = list(range(0,28)) # Runs for which we want to calculate the error
+ls_process_runs = list(range(0,48)) # Runs for which we want to calculate the error
 seq.generate_predictions_pickle_file(SYSTEM_NO,state_only =True,ls_process_runs=ls_process_runs)
 seq.generate_df_error(SYSTEM_NO,ls_process_runs)
 
--- microtensor_run.sh
+++ microtensor_run.sh
@@ -6,11 +6,7 @@ mkdir Run_info
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [x_dict] [x_layers] [x_nodes] [y_dict] [y_layers] [y_nodes] [xy_dict] [xy_layers] [xy_nodes] [write_to_file] 
 python3 ocdeepDMD_Sequential.py '/cpu:0' 90 0  3 8 6 1 1 1 1 1 1 > Run_info/SYS_90_RUN_0.txt &
 wait 
-python3 ocdeepDMD_Sequential.py '/cpu:0' 90 1  3 8 8 1 1 1 1 1 1 > Run_info/SYS_90_RUN_1.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/cpu:0' 90 2  4 9 6 1 1 1 1 1 1 > Run_info/SYS_90_RUN_2.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/cpu:0' 90 3  4 9 8 1 1 1 1 1 1 > Run_info/SYS_90_RUN_3.txt &
+python3 ocdeepDMD_Sequential.py '/cpu:0' 90 1  3 8 6 1 1 1 1 1 1 > Run_info/SYS_90_RUN_1.txt &
 wait 
 wait 
 echo "All sessions are complete" 
