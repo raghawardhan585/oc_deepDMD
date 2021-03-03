048055414566eba875828ef5fe80f6ec3307c650
raghawardhan585 <raghawardhan585@gmail.com>
2021-03-03 11:34:24 -0800

    Next runs

--- 1_SimulatedSystems_DataGeneration.py
+++ 1_SimulatedSystems_DataGeneration.py
@@ -169,7 +169,10 @@ oc.plot_training_valid_test_states(SYSTEM_NO)
 
 ## EMBEDDED DATA OF THE ABOVE EXAMPLE
 
-SYSTEM_NO = 62
+# SYSTEM_NO = 62
+# EMBEDDING = 4
+SYSTEM_NO = 63
+EMBEDDING = 5
 storage_folder = '/Users/shara/Box/YeungLabUCSBShare/Shara/DoE_Pputida_RNASeq_DataProcessing' + '/System_' + str(SYSTEM_NO)
 if os.path.exists(storage_folder):
     shutil.rmtree(storage_folder)
@@ -185,7 +188,7 @@ else:
 with open('/Users/shara/Box/YeungLabUCSBShare/Shara/DoE_Pputida_RNASeq_DataProcessing/System_61/System_61_SimulatedData.pickle', 'rb') as handle:
     dict_indexed_data = pickle.load(handle)
 N_CURVES = 300
-oc.sort_to_DMD_folder(storage_folder, N_CURVES, dict_indexed_data,SYSTEM_NO,EMBEDDING_NUMBER = 4)
+oc.sort_to_DMD_folder(storage_folder, N_CURVES, dict_indexed_data,SYSTEM_NO,EMBEDDING_NUMBER = EMBEDDING )
 
 
 
--- goldentensor_run.sh
+++ goldentensor_run.sh
@@ -3,17 +3,14 @@ rm -rf _current_run_saved_files
 mkdir _current_run_saved_files 
 rm -rf Run_info 
 mkdir Run_info 
-# Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [n_layers] [n_nodes] [write_to_file] 
-python3 direct_nn_identification.py '/gpu:0' 91 0 6 3 > Run_info/SYS_91_RUN_0.txt &
-python3 direct_nn_identification.py '/gpu:1' 91 1 6 4 > Run_info/SYS_91_RUN_1.txt &
-python3 direct_nn_identification.py '/gpu:2' 91 2 6 5 > Run_info/SYS_91_RUN_2.txt &
-python3 direct_nn_identification.py '/gpu:3' 91 3 6 6 > Run_info/SYS_91_RUN_3.txt &
+# Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [n_observables] [n_layers] [n_nodes] [write_to_file] 
+python3 deepDMD.py '/gpu:0' 63 0 4 3 6 > Run_info/SYS_63_RUN_0.txt &
+python3 deepDMD.py '/gpu:1' 63 1 4 3 9 > Run_info/SYS_63_RUN_1.txt &
+python3 deepDMD.py '/gpu:2' 63 2 4 4 6 > Run_info/SYS_63_RUN_2.txt &
+python3 deepDMD.py '/gpu:3' 63 3 4 4 9 > Run_info/SYS_63_RUN_3.txt &
 wait 
-python3 direct_nn_identification.py '/gpu:0' 91 4 8 3 > Run_info/SYS_91_RUN_4.txt &
-python3 direct_nn_identification.py '/gpu:1' 91 5 8 4 > Run_info/SYS_91_RUN_5.txt &
-python3 direct_nn_identification.py '/gpu:2' 91 6 8 5 > Run_info/SYS_91_RUN_6.txt &
-python3 direct_nn_identification.py '/gpu:3' 91 7 8 6 > Run_info/SYS_91_RUN_7.txt &
-wait 
+python3 deepDMD.py '/gpu:0' 63 4 5 8 6 > Run_info/SYS_63_RUN_4.txt &
+python3 deepDMD.py '/gpu:1' 63 5 5 8 9 > Run_info/SYS_63_RUN_5.txt &
 wait 
 echo "All sessions are complete" 
 echo "=======================================================" 
