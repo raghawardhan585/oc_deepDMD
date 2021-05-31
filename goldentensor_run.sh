5499fcbebd394844dec48c720e2317f4b1c45036
raghawardhan585 <raghawardhan585@gmail.com>
2021-05-30 19:35:32 -0700

    Next runs

--- goldentensor_run.sh
+++ goldentensor_run.sh
@@ -4,8 +4,8 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [x_dict] [x_layers] [x_nodes] [y_dict] [y_layers] [y_nodes] [xy_dict] [xy_layers] [xy_nodes] [regularization lambda] [write_to_file] 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 304 0  10 3 10 1 1 1 1 1 1 0.000375 > Run_info/SYS_304_RUN_0.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 304 1  10 3 10 1 1 1 1 1 1 0.000375 > Run_info/SYS_304_RUN_1.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:0' 304 0  8 3 10 1 1 1 1 1 1 0.000375 > Run_info/SYS_304_RUN_0.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:1' 304 1  8 3 10 1 1 1 1 1 1 0.000375 > Run_info/SYS_304_RUN_1.txt &
 python3 ocdeepDMD_Sequential.py '/gpu:2' 304 2  10 3 10 1 1 1 1 1 1 0.000375 > Run_info/SYS_304_RUN_2.txt &
 python3 ocdeepDMD_Sequential.py '/gpu:3' 304 3  10 3 10 1 1 1 1 1 1 0.000375 > Run_info/SYS_304_RUN_3.txt &
 wait 
--- ocdeepDMD_Sequential.py
+++ ocdeepDMD_Sequential.py
@@ -98,11 +98,11 @@ if RUN_OPTIMIZATION ==3:
 
 # Learning Parameters
 ls_dict_training_params = []
-dict_training_params = {'step_size_val': 00.5, 'train_error_threshold': float(1e-20),'valid_error_threshold': float(1e-6), 'max_epochs': 15000, 'batch_size': 86}
+dict_training_params = {'step_size_val': 00.5, 'train_error_threshold': float(1e-20),'valid_error_threshold': float(1e-6), 'max_epochs': 30000, 'batch_size': 86}
 ls_dict_training_params.append(dict_training_params)
-dict_training_params = {'step_size_val': 00.3, 'train_error_threshold': float(1e-20),'valid_error_threshold': float(1e-6), 'max_epochs': 15000, 'batch_size': 78}
+dict_training_params = {'step_size_val': 00.3, 'train_error_threshold': float(1e-20),'valid_error_threshold': float(1e-6), 'max_epochs': 30000, 'batch_size': 86}
 ls_dict_training_params.append(dict_training_params)
-dict_training_params = {'step_size_val': 0.1, 'train_error_threshold': float(1e-7), 'valid_error_threshold': float(1e-7), 'max_epochs': 15000, 'batch_size': 78}
+dict_training_params = {'step_size_val': 0.1, 'train_error_threshold': float(1e-7), 'valid_error_threshold': float(1e-7), 'max_epochs': 30000, 'batch_size': 86}
 ls_dict_training_params.append(dict_training_params)
 # dict_training_params = {'step_size_val': 0.09, 'train_error_threshold': float(1e-8), 'valid_error_threshold': float(1e-8), 'max_epochs': 30000, 'batch_size': 2000}
 # ls_dict_training_params.append(dict_training_params)
