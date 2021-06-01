f65068fd9a81de3c6aece1dc11c8be864d330b93
raghawardhan585 <raghawardhan585@gmail.com>
2021-06-01 08:15:18 -0700

    Next runs

--- goldentensor_run.sh
+++ goldentensor_run.sh
@@ -6,28 +6,18 @@ mkdir Run_info
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [x_dict] [x_layers] [x_nodes] [y_dict] [y_layers] [y_nodes] [xy_dict] [xy_layers] [xy_nodes] [regularization lambda] [write_to_file] 
 python3 ocdeepDMD_Sequential.py '/gpu:0' 400 0  0 1 0 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_0.txt &
 python3 ocdeepDMD_Sequential.py '/gpu:1' 400 1  0 1 0 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_1.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 400 2  2 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_2.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 400 3  2 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_3.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:2' 400 2  2 3 15 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_2.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:3' 400 3  2 3 15 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_3.txt &
 wait 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 400 4  4 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_4.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 400 5  4 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_5.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 400 6  8 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_6.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 400 7  8 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_7.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:0' 400 4  4 3 15 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_4.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:1' 400 5  4 3 15 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_5.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:2' 400 6  8 3 15 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_6.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:3' 400 7  8 3 15 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_7.txt &
 wait 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 400 8  10 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_8.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 400 9  10 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_9.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 400 10  12 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_10.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 400 11  12 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_11.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 400 12  15 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_12.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 400 13  15 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_13.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 400 14  18 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_14.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 400 15  18 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_15.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 400 16  20 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_16.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 400 17  20 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_17.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 400 18  25 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_18.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 400 19  25 3 25 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_19.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:0' 400 8  10 3 15 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_8.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:1' 400 9  10 3 15 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_9.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:2' 400 10  15 3 15 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_10.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:3' 400 11  15 3 15 1 1 1 1 1 1 6e-05 > Run_info/SYS_400_RUN_11.txt &
 wait 
 wait 
 echo "All sessions are complete" 
--- koopman_data/RNAseq_schedule_runs.py
+++ koopman_data/RNAseq_schedule_runs.py
@@ -33,9 +33,9 @@ NO_OF_ITERATIONS_IN_CPU = 2
 
 dict_hp={}
 dict_hp['x']={}
-dict_hp['x']['ls_dict_size'] = [0,0,2,2,4,4,8,8,10,10,12,12,15,15,18,18,20,20,25,25]
+dict_hp['x']['ls_dict_size'] = [0,0,2,2,4,4,8,8,10,10,15,15]
 dict_hp['x']['ls_nn_layers'] = [3]
-dict_hp['x']['ls_nn_nodes'] = [25]
+dict_hp['x']['ls_nn_nodes'] = [15]
 dict_hp['y']={}
 dict_hp['y']['ls_dict_size'] = [1,1]
 dict_hp['y']['ls_nn_layers'] = [8,9]
