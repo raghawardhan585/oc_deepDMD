4761853bf6f72c91f99bec5efd2a14fa3c49bac9
raghawardhan585 <raghawardhan585@gmail.com>
2021-06-01 13:36:50 -0700

    Next runs

---
+++ print_hyperparam_stats.py
@@ -0,0 +1,24 @@
+import pickle
+import pandas as pd
+import os
+
+file_path = '_current_run_saved_files'
+dict_run = {}
+for folder in os.listdir(file_path):
+    try:
+        with open(file_path + '/' + folder + '/' + 'dict_hyperparameters.pickle','rb') as handle:
+            dict_hp = pickle.load(handle)
+        try:
+            run_no = int(folder[-2:])
+        except:
+            run_no = int(folder[-1])
+        dict_run[run_no] = {'run_no': run_no, 'r2_train': dict_hp['r2 train'],
+                                 'r2_valid': dict_hp['r2 valid'],
+                                 'lambda': dict_hp['regularization factor'], 'x_obs': dict_hp['x_obs'], 'n_l & n_n': [dict_hp['x_layers'],dict_hp['x_nodes']] }
+    except:
+        print('Folder name: ', folder, ' is not a run')
+print('=====================================================================')
+print('Error Stats')
+print('=====================================================================')
+print(pd.DataFrame(dict_run).T.loc[:,['run_no', 'x_obs', 'r2_train', 'r2_valid', 'lambda', 'n_l & n_n']].sort_values(by=['x_obs']))
+print('=====================================================================')
