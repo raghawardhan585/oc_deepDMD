b9a823ccbba40fdd06e58f3b5523da9e0960c156
raghawardhan585 <raghawardhan585@gmail.com>
2021-06-01 17:42:09 -0700

    Next runs

--- ocdeepDMD_Sequential.py
+++ ocdeepDMD_Sequential.py
@@ -98,7 +98,7 @@ if RUN_OPTIMIZATION ==3:
 
 # Learning Parameters
 ls_dict_training_params = []
-dict_training_params = {'step_size_val': 00.5, 'train_error_threshold': float(1e-20),'valid_error_threshold': float(1e-6), 'max_epochs': 200, 'batch_size': 48} #20000
+dict_training_params = {'step_size_val': 00.5, 'train_error_threshold': float(1e-20),'valid_error_threshold': float(1e-6), 'max_epochs': 5000, 'batch_size': 48} #20000
 ls_dict_training_params.append(dict_training_params)
 # dict_training_params = {'step_size_val': 00.3, 'train_error_threshold': float(1e-20),'valid_error_threshold': float(1e-6), 'max_epochs': 5000, 'batch_size': 48}
 # ls_dict_training_params.append(dict_training_params)
