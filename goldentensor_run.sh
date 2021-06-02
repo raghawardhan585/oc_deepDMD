c4e92fd81c8ea8e839f33d7934564c04f9f18b86
raghawardhan585 <raghawardhan585@gmail.com>
2021-06-01 17:54:52 -0700

    Next runs

--- goldentensor_run.sh
+++ goldentensor_run.sh
@@ -4,30 +4,10 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [x_dict] [x_layers] [x_nodes] [y_dict] [y_layers] [y_nodes] [xy_dict] [xy_layers] [xy_nodes] [regularization lambda] [write_to_file] 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 400 0  0 1 0 1 1 1 1 1 1 4e-06 > Run_info/SYS_400_RUN_0.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 400 1  0 1 0 1 1 1 1 1 1 4.01e-06 > Run_info/SYS_400_RUN_1.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 400 2  0 1 0 1 1 1 1 1 1 4.02e-06 > Run_info/SYS_400_RUN_2.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 400 3  0 1 0 1 1 1 1 1 1 4.0299999999999995e-06 > Run_info/SYS_400_RUN_3.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 400 4  0 1 0 1 1 1 1 1 1 4.0399999999999994e-06 > Run_info/SYS_400_RUN_4.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 400 5  0 1 0 1 1 1 1 1 1 4.049999999999999e-06 > Run_info/SYS_400_RUN_5.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 400 6  0 1 0 1 1 1 1 1 1 4.059999999999999e-06 > Run_info/SYS_400_RUN_6.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 400 7  0 1 0 1 1 1 1 1 1 4.069999999999999e-06 > Run_info/SYS_400_RUN_7.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 400 8  0 1 0 1 1 1 1 1 1 4.079999999999999e-06 > Run_info/SYS_400_RUN_8.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 400 9  0 1 0 1 1 1 1 1 1 4.089999999999999e-06 > Run_info/SYS_400_RUN_9.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 400 10  0 1 0 1 1 1 1 1 1 4.099999999999999e-06 > Run_info/SYS_400_RUN_10.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 400 11  0 1 0 1 1 1 1 1 1 4.109999999999999e-06 > Run_info/SYS_400_RUN_11.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 400 12  0 1 0 1 1 1 1 1 1 4.119999999999999e-06 > Run_info/SYS_400_RUN_12.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 400 13  0 1 0 1 1 1 1 1 1 4.1299999999999986e-06 > Run_info/SYS_400_RUN_13.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 400 14  0 1 0 1 1 1 1 1 1 4.1399999999999985e-06 > Run_info/SYS_400_RUN_14.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 400 15  0 1 0 1 1 1 1 1 1 4.149999999999998e-06 > Run_info/SYS_400_RUN_15.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 400 16  0 1 0 1 1 1 1 1 1 4.159999999999998e-06 > Run_info/SYS_400_RUN_16.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 400 17  0 1 0 1 1 1 1 1 1 4.169999999999998e-06 > Run_info/SYS_400_RUN_17.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 400 18  0 1 0 1 1 1 1 1 1 4.179999999999998e-06 > Run_info/SYS_400_RUN_18.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 400 19  0 1 0 1 1 1 1 1 1 4.189999999999998e-06 > Run_info/SYS_400_RUN_19.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:0' 400 0  0 1 0 1 1 1 1 1 1 0 > Run_info/SYS_400_RUN_0.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:1' 400 1  0 1 0 1 1 1 1 1 1 0 > Run_info/SYS_400_RUN_1.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:2' 400 2  0 1 0 1 1 1 1 1 1 4.14e-06 > Run_info/SYS_400_RUN_2.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:3' 400 3  0 1 0 1 1 1 1 1 1 4.14e-06 > Run_info/SYS_400_RUN_3.txt &
 wait 
 wait 
 echo "All sessions are complete" 
