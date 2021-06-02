d488083a6e60428fcbc73e0b27c3aac82b8da110
raghawardhan585 <raghawardhan585@gmail.com>
2021-06-01 19:26:30 -0700

    Next runs

--- print_hyperparam_stats.py
+++ print_hyperparam_stats.py
@@ -14,9 +14,14 @@ for folder in os.listdir(file_path):
             run_no = int(folder[-2:])
         except:
             run_no = int(folder[-1])
+        # dict_run[run_no] = {'run_no': run_no, 'r2_train': dict_hp['r2 train'],
+        #                     'r2_valid': dict_hp['r2 valid'],
+        #                     'lambda': dict_hp['regularization factor'], 'x_obs': dict_hp['x_obs'],
+        #                     'n_l & n_n': [dict_hp['x_layers'], dict_hp['x_nodes']]}
         dict_run[run_no] = {'run_no': run_no, 'r2_train': dict_hp['r2 train'],
-                                 'r2_valid': dict_hp['r2 valid'],
-                                 'lambda': dict_hp['regularization factor'], 'x_obs': dict_hp['x_obs'], 'n_l & n_n': [dict_hp['x_layers'],dict_hp['x_nodes']] }
+                            'r2_valid': dict_hp['r2 valid'],
+                            'lambda': dict_hp['regularization factor'], 'y_obs': dict_hp['x_obs'],
+                            'n_l & n_n': [dict_hp['y_layers'], dict_hp['y_nodes']]}
         # dict_run[run_no] = {'run_no': run_no, 'lambda': dict_hp['regularization factor'], 'x_obs': dict_hp['x_obs'], 'n_l & n_n': [dict_hp['x_layers'],dict_hp['x_nodes']] }
     except:
         print('Folder name: ', folder, ' is not a run')
