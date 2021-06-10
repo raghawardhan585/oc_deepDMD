9f2d4b2b56a1db76fa9c05cb1945977e11dc914e
raghawardhan585 <raghawardhan585@gmail.com>
2021-06-10 02:10:11 -0700

    Next runs

--- goldentensor_run.sh
+++ goldentensor_run.sh
@@ -4,25 +4,25 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [n_observables] [n_layers] [n_nodes] [write_to_file] 
-python3 deepDMD.py '/gpu:0' 600 0 0 4 5 > Run_info/SYS_600_RUN_0.txt &
-python3 deepDMD.py '/gpu:1' 600 1 0 4 10 > Run_info/SYS_600_RUN_1.txt &
-python3 deepDMD.py '/gpu:2' 600 2 1 4 5 > Run_info/SYS_600_RUN_2.txt &
-python3 deepDMD.py '/gpu:3' 600 3 1 4 10 > Run_info/SYS_600_RUN_3.txt &
+python3 deepDMD.py '/gpu:0' 601 0 0 4 5 > Run_info/SYS_601_RUN_0.txt &
+python3 deepDMD.py '/gpu:1' 601 1 0 4 10 > Run_info/SYS_601_RUN_1.txt &
+python3 deepDMD.py '/gpu:2' 601 2 1 4 5 > Run_info/SYS_601_RUN_2.txt &
+python3 deepDMD.py '/gpu:3' 601 3 1 4 10 > Run_info/SYS_601_RUN_3.txt &
 wait 
-python3 deepDMD.py '/gpu:0' 600 4 2 4 5 > Run_info/SYS_600_RUN_4.txt &
-python3 deepDMD.py '/gpu:1' 600 5 2 4 10 > Run_info/SYS_600_RUN_5.txt &
-python3 deepDMD.py '/gpu:2' 600 6 3 4 5 > Run_info/SYS_600_RUN_6.txt &
-python3 deepDMD.py '/gpu:3' 600 7 3 4 10 > Run_info/SYS_600_RUN_7.txt &
+python3 deepDMD.py '/gpu:0' 601 4 2 4 5 > Run_info/SYS_601_RUN_4.txt &
+python3 deepDMD.py '/gpu:1' 601 5 2 4 10 > Run_info/SYS_601_RUN_5.txt &
+python3 deepDMD.py '/gpu:2' 601 6 3 4 5 > Run_info/SYS_601_RUN_6.txt &
+python3 deepDMD.py '/gpu:3' 601 7 3 4 10 > Run_info/SYS_601_RUN_7.txt &
 wait 
-python3 deepDMD.py '/gpu:0' 600 8 4 4 5 > Run_info/SYS_600_RUN_8.txt &
-python3 deepDMD.py '/gpu:1' 600 9 4 4 10 > Run_info/SYS_600_RUN_9.txt &
-python3 deepDMD.py '/gpu:2' 600 10 5 4 5 > Run_info/SYS_600_RUN_10.txt &
-python3 deepDMD.py '/gpu:3' 600 11 5 4 10 > Run_info/SYS_600_RUN_11.txt &
+python3 deepDMD.py '/gpu:0' 601 8 4 4 5 > Run_info/SYS_601_RUN_8.txt &
+python3 deepDMD.py '/gpu:1' 601 9 4 4 10 > Run_info/SYS_601_RUN_9.txt &
+python3 deepDMD.py '/gpu:2' 601 10 5 4 5 > Run_info/SYS_601_RUN_10.txt &
+python3 deepDMD.py '/gpu:3' 601 11 5 4 10 > Run_info/SYS_601_RUN_11.txt &
 wait 
-python3 deepDMD.py '/gpu:0' 600 12 6 4 5 > Run_info/SYS_600_RUN_12.txt &
-python3 deepDMD.py '/gpu:1' 600 13 6 4 10 > Run_info/SYS_600_RUN_13.txt &
-python3 deepDMD.py '/gpu:2' 600 14 7 4 5 > Run_info/SYS_600_RUN_14.txt &
-python3 deepDMD.py '/gpu:3' 600 15 7 4 10 > Run_info/SYS_600_RUN_15.txt &
+python3 deepDMD.py '/gpu:0' 601 12 6 4 5 > Run_info/SYS_601_RUN_12.txt &
+python3 deepDMD.py '/gpu:1' 601 13 6 4 10 > Run_info/SYS_601_RUN_13.txt &
+python3 deepDMD.py '/gpu:2' 601 14 7 4 5 > Run_info/SYS_601_RUN_14.txt &
+python3 deepDMD.py '/gpu:3' 601 15 7 4 10 > Run_info/SYS_601_RUN_15.txt &
 wait 
 wait 
 echo "All sessions are complete" 
