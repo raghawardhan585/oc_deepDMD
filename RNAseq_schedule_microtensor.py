926d6e3dff24034e4a51a6e86b58115c66d67026
raghawardhan585 <raghawardhan585@gmail.com>
2021-06-17 20:47:55 -0700

    Next runs

--- RNAseq_schedule_microtensor.py
+++ RNAseq_schedule_microtensor.py
@@ -5,13 +5,13 @@ import itertools
 
 
 ## Bash Script Generation
-DATA_SYSTEM_TO_WRITE_BASH_SCRIPT_FOR = 401
+DATA_SYSTEM_TO_WRITE_BASH_SCRIPT_FOR = 701
 
 dict_hp={}
 dict_hp['x']={}
-dict_hp['x']['ls_dict_size'] = [1,2]
-dict_hp['x']['ls_nn_layers'] = [3,4]
-dict_hp['x']['ls_nn_nodes'] = [50]
+dict_hp['x']['ls_dict_size'] = [1]
+dict_hp['x']['ls_nn_layers'] = [5]
+dict_hp['x']['ls_nn_nodes'] = [10]
 dict_hp['y']={}
 dict_hp['y']['ls_dict_size'] = [20]
 dict_hp['y']['ls_nn_layers'] = [3,4]
@@ -51,43 +51,43 @@ mt = open('/Users/shara/Desktop/oc_deepDMD/microtensor_run.sh','w')
 mt.write('#!/bin/bash \n')
 
 # Scheduling Lasso Regression
-mt.write('python3 RNAseq_LassoModel_Y.py > System_401/Lasso_Regression_Y/LassoRun.txt &\n')
-mt.write('wait \n')
-mt.write('echo "All sessions are complete" \n')
-mt.write('echo "=======================================================" \n')
-mt.close()
-
-# Scheduling oc deepDMD runs
-# mt.write('rm -rf _current_run_saved_files \n')
-# mt.write('mkdir _current_run_saved_files \n')
-# mt.write('rm -rf Run_info \n')
-# mt.write('mkdir Run_info \n')
-# mt.write('# Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [x_dict] [x_layers] [x_nodes] [y_dict] [y_layers] [y_nodes] [xy_dict] [xy_layers] [xy_nodes] [regularization lambda] [write_to_file] \n')
-#
-# run_no = 0
-# for i in dict_all_run_conditions.keys():
-#     run_params = ''
-#     for items in ['x','y','xy']:
-#         for sub_items in dict_all_run_conditions[i][items].keys():
-#             run_params = run_params + ' ' + str(dict_all_run_conditions[i][items][sub_items])
-#     run_params = run_params + ' ' + str(dict_all_run_conditions[i]['regularization lambda'])
-#     if np.mod(i,10) ==0 or np.mod(i,10) ==1: # Microtensor CPU 0
-#         general_run = 'python3 ocdeepDMD_Sequential.py \'/cpu:0\' ' + str(SYSTEM_NO) + ' ' + str(run_no) + ' '
-#         write_to_file = ' > Run_info/SYS_' + str(SYSTEM_NO) + '_RUN_' + str(run_no) + '.txt &\n'
-#         mt.write(general_run + run_params + write_to_file)
-#         mt.write('wait \n')
-#         run_no = run_no + 1
-#
+# mt.write('python3 RNAseq_LassoModel_Y.py > System_401/Lasso_Regression_Y/LassoRun.txt &\n')
 # mt.write('wait \n')
 # mt.write('echo "All sessions are complete" \n')
 # mt.write('echo "=======================================================" \n')
-# mt.write('cd .. \n')
-# mt.write('rm -R _current_run_saved_files \n')
-# mt.write('rm -R Run_info \n')
-# mt.write('cp -a oc_deepDMD/_current_run_saved_files/. _current_run_saved_files \n')
-# mt.write('cp -a oc_deepDMD/Run_info/ Run_info \n')
-# mt.write('cd oc_deepDMD/ \n')
 # mt.close()
+
+# Scheduling oc deepDMD runs
+mt.write('rm -rf _current_run_saved_files \n')
+mt.write('mkdir _current_run_saved_files \n')
+mt.write('rm -rf Run_info \n')
+mt.write('mkdir Run_info \n')
+mt.write('# Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [x_dict] [x_layers] [x_nodes] [y_dict] [y_layers] [y_nodes] [xy_dict] [xy_layers] [xy_nodes] [regularization lambda] [write_to_file] \n')
+
+run_no = 0
+for i in dict_all_run_conditions.keys():
+    run_params = ''
+    for items in ['x','y','xy']:
+        for sub_items in dict_all_run_conditions[i][items].keys():
+            run_params = run_params + ' ' + str(dict_all_run_conditions[i][items][sub_items])
+    run_params = run_params + ' ' + str(dict_all_run_conditions[i]['regularization lambda'])
+    # Microtensor CPU 0
+    general_run = 'python3 ocdeepDMD_Sequential.py \'/cpu:0\' ' + str(SYSTEM_NO) + ' ' + str(run_no) + ' '
+    write_to_file = ' > Run_info/SYS_' + str(SYSTEM_NO) + '_RUN_' + str(run_no) + '.txt &\n'
+    mt.write(general_run + run_params + write_to_file)
+    mt.write('wait \n')
+    run_no = run_no + 1
+
+mt.write('wait \n')
+mt.write('echo "All sessions are complete" \n')
+mt.write('echo "=======================================================" \n')
+mt.write('cd .. \n')
+mt.write('rm -R _current_run_saved_files \n')
+mt.write('rm -R Run_info \n')
+mt.write('cp -a oc_deepDMD/_current_run_saved_files/. _current_run_saved_files \n')
+mt.write('cp -a oc_deepDMD/Run_info/ Run_info \n')
+mt.write('cd oc_deepDMD/ \n')
+mt.close()
 
 
 ## Transfer the oc deepDMD files
--- microtensor_run.sh
+++ microtensor_run.sh
@@ -4,6 +4,22 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [x_dict] [x_layers] [x_nodes] [y_dict] [y_layers] [y_nodes] [xy_dict] [xy_layers] [xy_nodes] [regularization lambda] [write_to_file] 
+python3 ocdeepDMD_Sequential.py '/cpu:0' 701 0  1 5 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_0.txt &
+wait 
+python3 ocdeepDMD_Sequential.py '/cpu:0' 701 1  2 5 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_1.txt &
+wait 
+python3 ocdeepDMD_Sequential.py '/cpu:0' 701 2  3 5 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_2.txt &
+wait 
+python3 ocdeepDMD_Sequential.py '/cpu:0' 701 3  4 5 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_3.txt &
+wait 
+python3 ocdeepDMD_Sequential.py '/cpu:0' 701 4  5 5 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_4.txt &
+wait 
+python3 ocdeepDMD_Sequential.py '/cpu:0' 701 5  6 5 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_5.txt &
+wait 
+python3 ocdeepDMD_Sequential.py '/cpu:0' 701 6  7 5 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_6.txt &
+wait 
+python3 ocdeepDMD_Sequential.py '/cpu:0' 701 7  8 5 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_7.txt &
+wait 
 wait 
 echo "All sessions are complete" 
 echo "=======================================================" 
