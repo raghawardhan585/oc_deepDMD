2be8633429dadc4be273fb8e16a2b21e23f03e1b
raghawardhan585 <raghawardhan585@gmail.com>
2021-06-18 14:24:36 -0700

    Next runs

--- RNAseq_DirectDeepDMD_goldentensor.py
+++ RNAseq_DirectDeepDMD_goldentensor.py
@@ -27,14 +27,14 @@ colors = np.asarray(colors);  # defines a color palette
 
 
 ## Bash Script Generation
-DATA_SYSTEM_TO_WRITE_BASH_SCRIPT_FOR = 702#406
+DATA_SYSTEM_TO_WRITE_BASH_SCRIPT_FOR = 703#406
 NO_OF_ITERATIONS_PER_GPU = 1
 NO_OF_ITERATIONS_IN_CPU = 1
 
 dict_hp={}
-dict_hp['ls_dict_size'] = [0,1,2,3,4,5,6,7,8,9]
+dict_hp['ls_dict_size'] = [0,1,2,3,4,5,6,7]
 dict_hp['ls_nn_layers'] = [4]
-dict_hp['ls_nn_nodes'] = [10,10]
+dict_hp['ls_nn_nodes'] = [10,10,10]
 SYSTEM_NO = DATA_SYSTEM_TO_WRITE_BASH_SCRIPT_FOR
 
 ls_dict_size = dict_hp['ls_dict_size']
