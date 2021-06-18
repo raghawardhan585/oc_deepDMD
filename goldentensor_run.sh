6906eaefc5efa97512c4ad2b58ecb97842bbc030
raghawardhan585 <raghawardhan585@gmail.com>
2021-06-17 21:48:16 -0700

    Next runs

--- goldentensor_run.sh
+++ goldentensor_run.sh
@@ -4,15 +4,15 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [x_dict] [x_layers] [x_nodes] [y_dict] [y_layers] [y_nodes] [xy_dict] [xy_layers] [xy_nodes] [regularization lambda] [write_to_file] 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 702 0  0 1 0 1 1 1 1 1 1 0 > Run_info/SYS_702_RUN_0.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 702 1  0 1 0 1 1 1 1 1 1 0 > Run_info/SYS_702_RUN_1.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 702 2  1 4 10 1 1 1 1 1 1 0 > Run_info/SYS_702_RUN_2.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 702 3  1 4 10 1 1 1 1 1 1 0 > Run_info/SYS_702_RUN_3.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:0' 701 0  1 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_0.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:1' 701 1  1 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_1.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:2' 701 2  2 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_2.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:3' 701 3  2 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_3.txt &
 wait 
-python3 ocdeepDMD_Sequential.py '/gpu:0' 702 4  2 4 10 1 1 1 1 1 1 0 > Run_info/SYS_702_RUN_4.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:1' 702 5  2 4 10 1 1 1 1 1 1 0 > Run_info/SYS_702_RUN_5.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:2' 702 6  3 4 10 1 1 1 1 1 1 0 > Run_info/SYS_702_RUN_6.txt &
-python3 ocdeepDMD_Sequential.py '/gpu:3' 702 7  3 4 10 1 1 1 1 1 1 0 > Run_info/SYS_702_RUN_7.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:0' 701 4  3 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_4.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:1' 701 5  3 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_5.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:2' 701 6  4 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_6.txt &
+python3 ocdeepDMD_Sequential.py '/gpu:3' 701 7  4 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_7.txt &
 wait 
 wait 
 echo "All sessions are complete" 
--- microtensor_run.sh
+++ microtensor_run.sh
@@ -4,22 +4,6 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [x_dict] [x_layers] [x_nodes] [y_dict] [y_layers] [y_nodes] [xy_dict] [xy_layers] [xy_nodes] [regularization lambda] [write_to_file] 
-python3 ocdeepDMD_Sequential.py '/cpu:0' 701 0  1 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_0.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/cpu:0' 701 1  1 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_1.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/cpu:0' 701 2  2 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_2.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/cpu:0' 701 3  2 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_3.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/cpu:0' 701 4  3 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_4.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/cpu:0' 701 5  3 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_5.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/cpu:0' 701 6  4 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_6.txt &
-wait 
-python3 ocdeepDMD_Sequential.py '/cpu:0' 701 7  4 4 10 1 1 1 1 1 1 0 > Run_info/SYS_701_RUN_7.txt &
-wait 
 wait 
 echo "All sessions are complete" 
 echo "=======================================================" 
