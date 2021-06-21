b0e35f698dc96145c6dabe7e885fd9977ca66437
raghawardhan585 <raghawardhan585@gmail.com>
2021-06-21 13:04:04 -0700

    Next runs

--- RNAseq_main3.py
+++ RNAseq_main3.py
@@ -110,9 +110,6 @@ plot_gene_expression(dict_data_GO_filtered)
 # dict_data = rnaf.filter_gene_by_coefficient_of_variation(copy.deepcopy(dict_data_GO_filtered), CV_THRESHOLD = 0.0175,ALL_CONDITIONS=['MX'])
 # rnaf.formulate_and_save_Koopman_Data(dict_data,SYSTEM_NO= 703, ALL_CONDITIONS= ['MX'])
 
-# dict_data = rnaf.filter_gene_by_coefficient_of_variation(copy.deepcopy(dict_data_GO_filtered), CV_THRESHOLD = 0.0175,ALL_CONDITIONS=['MX','NC','MN'])
-# rnaf.formulate_and_save_Koopman_Data(dict_data,SYSTEM_NO= 704, ALL_CONDITIONS= ['MX','NC','MN'])
-
 dict_data = rnaf.filter_gene_by_coefficient_of_variation(copy.deepcopy(dict_data_GO_filtered), CV_THRESHOLD = 0.015,ALL_CONDITIONS=['MX'])
 rnaf.formulate_and_save_Koopman_Data(dict_data,SYSTEM_NO= 704, ALL_CONDITIONS= ['MX'])
 
