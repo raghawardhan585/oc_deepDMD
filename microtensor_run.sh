8923704a9c4aff3773e0fee346ae609995ff6d6e
raghawardhan585 <raghawardhan585@gmail.com>
2021-06-30 15:22:12 -0700

    Next runs

--- microtensor_run.sh
+++ microtensor_run.sh
@@ -4,37 +4,29 @@ mkdir _current_run_saved_files
 rm -rf Run_info 
 mkdir Run_info 
 # Gen syntax: [interpreter] [code.py] [device] [sys_no] [run_no] [n_observables] [n_layers] [n_nodes] [write_to_file] 
-python3 deepDMD.py '/cpu:0' 801 0 0 3 10 > Run_info/SYS_801_RUN_0.txt &
+python3 deepDMD.py '/cpu:0' 91 0 3 3 4 > Run_info/SYS_91_RUN_0.txt &
 wait 
-python3 deepDMD.py '/cpu:0' 801 1 0 3 10 > Run_info/SYS_801_RUN_1.txt &
+python3 deepDMD.py '/cpu:0' 91 1 3 3 5 > Run_info/SYS_91_RUN_1.txt &
 wait 
-python3 deepDMD.py '/cpu:0' 801 2 1 3 10 > Run_info/SYS_801_RUN_2.txt &
+python3 deepDMD.py '/cpu:0' 91 2 3 4 4 > Run_info/SYS_91_RUN_2.txt &
 wait 
-python3 deepDMD.py '/cpu:0' 801 3 1 3 10 > Run_info/SYS_801_RUN_3.txt &
+python3 deepDMD.py '/cpu:0' 91 3 3 4 5 > Run_info/SYS_91_RUN_3.txt &
 wait 
-python3 deepDMD.py '/cpu:0' 801 4 2 3 10 > Run_info/SYS_801_RUN_4.txt &
+python3 deepDMD.py '/cpu:0' 91 4 3 3 4 > Run_info/SYS_91_RUN_4.txt &
 wait 
-python3 deepDMD.py '/cpu:0' 801 5 2 3 10 > Run_info/SYS_801_RUN_5.txt &
+python3 deepDMD.py '/cpu:0' 91 5 3 3 5 > Run_info/SYS_91_RUN_5.txt &
 wait 
-python3 deepDMD.py '/cpu:0' 801 6 3 3 10 > Run_info/SYS_801_RUN_6.txt &
+python3 deepDMD.py '/cpu:0' 91 6 3 4 4 > Run_info/SYS_91_RUN_6.txt &
 wait 
-python3 deepDMD.py '/cpu:0' 801 7 3 3 10 > Run_info/SYS_801_RUN_7.txt &
+python3 deepDMD.py '/cpu:0' 91 7 3 4 5 > Run_info/SYS_91_RUN_7.txt &
 wait 
-python3 deepDMD.py '/cpu:0' 801 8 4 3 10 > Run_info/SYS_801_RUN_8.txt &
+python3 deepDMD.py '/cpu:0' 91 8 3 3 4 > Run_info/SYS_91_RUN_8.txt &
 wait 
-python3 deepDMD.py '/cpu:0' 801 9 4 3 10 > Run_info/SYS_801_RUN_9.txt &
+python3 deepDMD.py '/cpu:0' 91 9 3 3 5 > Run_info/SYS_91_RUN_9.txt &
 wait 
-python3 deepDMD.py '/cpu:0' 801 10 5 3 10 > Run_info/SYS_801_RUN_10.txt &
+python3 deepDMD.py '/cpu:0' 91 10 3 4 4 > Run_info/SYS_91_RUN_10.txt &
 wait 
-python3 deepDMD.py '/cpu:0' 801 11 5 3 10 > Run_info/SYS_801_RUN_11.txt &
-wait 
-python3 deepDMD.py '/cpu:0' 801 12 6 3 10 > Run_info/SYS_801_RUN_12.txt &
-wait 
-python3 deepDMD.py '/cpu:0' 801 13 6 3 10 > Run_info/SYS_801_RUN_13.txt &
-wait 
-python3 deepDMD.py '/cpu:0' 801 14 7 3 10 > Run_info/SYS_801_RUN_14.txt &
-wait 
-python3 deepDMD.py '/cpu:0' 801 15 7 3 10 > Run_info/SYS_801_RUN_15.txt &
+python3 deepDMD.py '/cpu:0' 91 11 3 4 5 > Run_info/SYS_91_RUN_11.txt &
 wait 
 wait 
 echo "All sessions are complete" 
